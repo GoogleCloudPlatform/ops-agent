8e86b91ba62738fc13f84396771d9c590f0a0d73
Igor Peshansky <igorp@google.com>
2020-09-30 17:58:12 -0400

    A minor cleanup of the Debian build script.
    b/163830226
    
    Change-Id: I170063f49fd1f95bceff26a1913f60977c183cd4

--- build-deb.sh
+++ build-deb.sh
@@ -3,8 +3,10 @@
 set -ex
 
 # Add changelog entry
-DEBEMAIL="google-cloud-ops-agent@google.com" DEBFULLNAME="Google Cloud Ops Agent" dch --create -b --package google-cloud-ops-agent \
-  --distribution `lsb_release -cs` -v $PKG_VERSION~$(lsb_release -is | tr A-Z a-z)$(lsb_release -rs) "Automated build"
+dch --create -b --package google-cloud-ops-agent -M \
+  --distribution $(lsb_release -cs) \
+  -v $PKG_VERSION~$(lsb_release -is | tr A-Z a-z)$(lsb_release -rs) \
+  "Automated build"
 
 # Build .debs
 debuild -us -uc -sa
