d0097661a193ddc0d26b72aec193a3f890687d26
Ling Huang <qingling128@users.noreply.github.com>
2021-02-12 17:28:04 -0500

    Update version to 1.0.2. (#35)

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ systemdsystempresetdir=$(pkg-config systemd --variable=systemdsystempresetdir)
 subagentdir=$prefix/subagents
 # TODO: Get version number from packaging
 if [ -z "$version" ]; then
-  version=1.0.1
+  version=1.0.2
 fi
 
 set -x -e
