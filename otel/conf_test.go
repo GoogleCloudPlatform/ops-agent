e325da91c875046ea0b04229b134985e4f541a75
Quentin Smith <quentin@google.com>
2021-04-28 02:59:13 -0400

    Fix more disk metrics

--- otel/conf_test.go
+++ otel/conf_test.go
@@ -327,6 +327,7 @@ processors:
         metric_names:
           - system.network.dropped
           - system.filesystem.inodes.usage
+          - system.paging.faults
 
   # convert from opentelemetry metric formats to cloud monitoring formats
   metricstransform/system:
@@ -409,9 +410,10 @@ processors:
       - metric_name: system.disk.pending_operations
         action: update
         new_name: disk/pending_operations
-        operations:
-          # change data type from int64 -> double
-          - action: toggle_scalar_data_type
+      # system.disk.merged -> disk/merged_operations
+      - metric_name: system.disk.merged
+        action: update
+        new_name: agent.googleapis.com/disk/merged_operations
       # system.filesystem.usage -> disk/bytes_used
       - metric_name: system.filesystem.usage
         action: update
