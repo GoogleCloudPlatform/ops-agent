81d08a7934f705aa414d627edf5fce67dff40a96
Igor Peshansky <igorp@google.com>
2021-07-31 16:09:55 -0400

    Fix typos in exporters warnings.

--- confgenerator/config.go
+++ confgenerator/config.go
@@ -210,7 +210,7 @@ func (l *Logging) Validate(platform string) error {
 		return err
 	}
 	if len(l.Exporters) > 0 {
-		log.Print(`The "metrics.exporters" field is no longer needed and will be ignored. This does not change any functionality. Please remove it from your configuration.`)
+		log.Print(`The "logging.exporters" field is no longer needed and will be ignored. This does not change any functionality. Please remove it from your configuration.`)
 	}
 	for id, r := range l.Receivers {
 		if err := r.ValidateType(subagent, "receiver", id, platform); err != nil {
@@ -318,7 +318,7 @@ func (m *Metrics) Validate(platform string) error {
 			return err
 		}
 		if len(p.ExporterIDs) > 0 {
-			log.Printf(`The "logging.service.pipelines.%s.exporters" field is deprecated and will be ignored. Please remove it from your configuration.`, id)
+			log.Printf(`The "metrics.service.pipelines.%s.exporters" field is deprecated and will be ignored. Please remove it from your configuration.`, id)
 		}
 	}
 	return nil
