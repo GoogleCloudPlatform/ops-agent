9cd5fd3b22fec19e958936b8d9d24ec6fce976f3
Quentin Smith <quentin@google.com>
2021-08-09 18:28:23 -0400

    Use the YAML field name in validation failures

--- confgenerator/config.go
+++ confgenerator/config.go
@@ -100,6 +100,13 @@ var platformKey = platformKeyType{}
 
 func newValidator() *validator.Validate {
 	v := validator.New()
+	v.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("yaml"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
 	v.RegisterValidation("duration", func(fl validator.FieldLevel) bool {
 		t, err := time.ParseDuration(fl.Field().String())
 		if err != nil {
