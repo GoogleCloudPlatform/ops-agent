acbcdd0b755acef1293f2d1538576282638f624a
Sam DeHaan <sam.dehaan@bluemedora.com>
2021-10-04 14:23:20 -0400

    Additional error log format

--- apps/apache.go
+++ apps/apache.go
@@ -88,6 +88,7 @@ func (p LoggingProcessorApacheError) Components(tag string, uid string) []fluent
 	c := confgenerator.LoggingProcessorParseRegex{
 		// Documentation: https://httpd.apache.org/docs/current/logs.html#errorlog
 		// Sample line 2.4: [Fri Sep 09 10:42:29.902022 2011] [core:error] [pid 35708:tid 4328636416] (13)Permission denied [client 72.15.99.187] File does not exist: /usr/local/apache2/htdocs/favicon.ico
+		// 					[Thu Sep 30 03:18:29.239182 2021] [ssl:error] [pid 2451:tid 140169666050176] AH02217: ssl_stapling_init_cert: Can't retrieve issuer certificate!
 		// Sample line 2.2: [Fri Sep 09 10:42:29.902022 2011] [error] [pid 35708:tid 4328636416] [client 72.15.99.187] File does not exist: /usr/local/apache2/htdocs/favicon.ico
 		// TODO - Support time parsing for version 2.0 where smallest resolution is seconds
 		// Sample line 2.0: [Wed Oct 11 14:32:52 2000] [error] [client 127.0.0.1] client denied by server configuration: /export/home/live/ap/htdocs/test
