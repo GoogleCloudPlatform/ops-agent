9d6010048f07a05b08f02d42c233a5b4d45ed507
Sam DeHaan <sam.dehaan@bluemedora.com>
2021-10-04 16:42:00 -0400

    Implement cassandra debug and gc

--- apps/cassandra.go
+++ apps/cassandra.go
@@ -30,6 +30,22 @@ func (LoggingProcessorCassandraSystem) Type() string {
 }
 
 func (p LoggingProcessorCassandraSystem) Components(tag string, uid string) []fluentbit.Component {
+	return javaLogParsingComponents(tag, uid)
+}
+
+type LoggingProcessorCassandraDebug struct {
+	confgenerator.ConfigComponent `yaml:",inline"`
+}
+
+func (LoggingProcessorCassandraDebug) Type() string {
+	return "cassandra_debug"
+}
+
+func (p LoggingProcessorCassandraDebug) Components(tag string, uid string) []fluentbit.Component {
+	return javaLogParsingComponents(tag, uid)
+}
+
+func javaLogParsingComponents(tag string, uid string) []fluentbit.Component {
 	c := confgenerator.LoggingProcessorParseMultilineRegex{
 		LoggingProcessorParseRegex: confgenerator.LoggingProcessorParseRegex{
 			Parsers: []confgenerator.RegexParser{
@@ -81,6 +97,49 @@ func (p LoggingProcessorCassandraSystem) Components(tag string, uid string) []fl
 	return c
 }
 
+type LoggingProcessorCassandraGC struct {
+	confgenerator.ConfigComponent `yaml:",inline"`
+}
+
+func (LoggingProcessorCassandraGC) Type() string {
+	return "cassandra_gc"
+}
+
+func (p LoggingProcessorCassandraGC) Components(tag string, uid string) []fluentbit.Component {
+	c := confgenerator.LoggingProcessorParseMultilineRegex{
+		LoggingProcessorParseRegex: confgenerator.LoggingProcessorParseRegex{
+			Parsers: []confgenerator.RegexParser{
+				confgenerator.RegexParser{
+					// Sample line: 2021-10-02T04:18:28.284+0000: 3.315: Total time for which application threads were stopped: 0.0002390 seconds, Stopping threads took: 0.0000281 seconds
+					// Lines may also contain more detailed GC Heap information in the following lines
+					Regex: `(?<time>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3,6}(?:Z|[+-]\d{2}:?\d{2})):\s+(?<uptime>\d+\.\d{3,6}):\s+(?<extendedMessage>(?<message>.*)[\s\S]+)`,
+					Parser: fluentbit.ParserShared{
+						TimeKey:    "time",
+						TimeFormat: "%Y-%m-%dT%H:%M:%S,%L%z",
+						Types: map[string]string{
+							"uptime": "float",
+						},
+					},
+				},
+			},
+		},
+		Rules: []confgenerator.MultilineRule{
+			confgenerator.MultilineRule{
+				StateName: "start_state",
+				NextState: "cont",
+				Regex:     `^\d{4}-\d{2}-\d{2}`,
+			},
+			confgenerator.MultilineRule{
+				StateName: "cont",
+				NextState: "cont",
+				Regex:     `^(?!\d{4}-\d{2}-\d{2})`,
+			},
+		},
+	}.Components(tag, uid)
+
+	return c
+}
+
 type LoggingReceiverCassandraSystem struct {
 	LoggingProcessorCassandraSystem         `yaml:",inline"`
 	confgenerator.LoggingReceiverFilesMixin `yaml:",inline" validate:"structonly"`
@@ -88,14 +147,52 @@ type LoggingReceiverCassandraSystem struct {
 
 func (r LoggingReceiverCassandraSystem) Components(tag string) []fluentbit.Component {
 	if len(r.IncludePaths) == 0 {
-		r.IncludePaths = []string{"/var/log/cassandra/system*.log"}
+		r.IncludePaths = []string{
+			"/var/log/cassandra/system*.log",
+		}
 	}
 	c := r.LoggingReceiverFilesMixin.Components(tag)
 	c = append(c, r.LoggingProcessorCassandraSystem.Components(tag, "cassandra_system")...)
 	return c
 }
 
+type LoggingReceiverCassandraDebug struct {
+	LoggingProcessorCassandraDebug          `yaml:",inline"`
+	confgenerator.LoggingReceiverFilesMixin `yaml:",inline" validate:"structonly"`
+}
+
+func (r LoggingReceiverCassandraDebug) Components(tag string) []fluentbit.Component {
+	if len(r.IncludePaths) == 0 {
+		r.IncludePaths = []string{
+			"/var/log/cassandra/debug*.log",
+		}
+	}
+	c := r.LoggingReceiverFilesMixin.Components(tag)
+	c = append(c, r.LoggingProcessorCassandraDebug.Components(tag, "cassandra_system")...)
+	return c
+}
+
+type LoggingReceiverCassandraGC struct {
+	LoggingProcessorCassandraGC             `yaml:",inline"`
+	confgenerator.LoggingReceiverFilesMixin `yaml:",inline" validate:"structonly"`
+}
+
+func (r LoggingReceiverCassandraGC) Components(tag string) []fluentbit.Component {
+	if len(r.IncludePaths) == 0 {
+		r.IncludePaths = []string{
+			"/var/log/cassandra/gc*.log.current",
+		}
+	}
+	c := r.LoggingReceiverFilesMixin.Components(tag)
+	c = append(c, r.LoggingProcessorCassandraGC.Components(tag, "cassandra_gc")...)
+	return c
+}
+
 func init() {
 	confgenerator.LoggingProcessorTypes.RegisterType(func() confgenerator.Component { return &LoggingProcessorCassandraSystem{} })
+	confgenerator.LoggingProcessorTypes.RegisterType(func() confgenerator.Component { return &LoggingProcessorCassandraDebug{} })
+	confgenerator.LoggingProcessorTypes.RegisterType(func() confgenerator.Component { return &LoggingProcessorCassandraGC{} })
 	confgenerator.LoggingReceiverTypes.RegisterType(func() confgenerator.Component { return &LoggingReceiverCassandraSystem{} })
+	confgenerator.LoggingReceiverTypes.RegisterType(func() confgenerator.Component { return &LoggingReceiverCassandraDebug{} })
+	confgenerator.LoggingReceiverTypes.RegisterType(func() confgenerator.Component { return &LoggingReceiverCassandraGC{} })
 }
