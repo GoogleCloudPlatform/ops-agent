3cca36f2205a1981caabf0fe9efa95d350925c0c
Quentin Smith <quentin@google.com>
2021-10-26 18:17:26 -0400

    Parse AND and OR

--- confgenerator/filter/internal/ast/ast.go
+++ confgenerator/filter/internal/ast/ast.go
@@ -47,14 +47,85 @@ func NewRestriction(lhs, operator, rhs Attrib) (*Restriction, error) {
 	return &r, nil
 }
 
+func (r Restriction) Simplify() Expression {
+	return r
+}
+
 type Expression interface {
+	Simplify() Expression
+}
+
+func Simplify(a Attrib) (Expression, error) {
+	switch a := a.(type) {
+	case Expression:
+		return a.Simplify(), nil
+	}
+	return nil, fmt.Errorf("expected expression: %v", a)
 }
 
 type Conjunction []Expression
 type Disjunction []Expression
 
+func NewConjunction(a Attrib) (Conjunction, error) {
+	switch a := a.(type) {
+	case Conjunction:
+		return a, nil
+	case Expression:
+		return Conjunction{a.Simplify()}, nil
+	}
+	return nil, fmt.Errorf("expected expression: %v", a)
+}
+
+func (c Conjunction) Simplify() Expression {
+	if len(c) == 1 {
+		return c[0]
+	}
+	return c
+}
+
+func (c Conjunction) Append(a Attrib) (Conjunction, error) {
+	switch a := a.(type) {
+	case Conjunction:
+		return append(c, a...), nil
+	case Expression:
+		return append(c, a.Simplify()), nil
+	}
+	return nil, fmt.Errorf("expected expression: %v", a)
+}
+
+func NewDisjunction(a Attrib) (Disjunction, error) {
+	switch a := a.(type) {
+	case Disjunction:
+		return a, nil
+	case Expression:
+		return Disjunction{a.Simplify()}, nil
+	}
+	return nil, fmt.Errorf("expected expression: %v", a)
+}
+
+func (d Disjunction) Simplify() Expression {
+	if len(d) == 1 {
+		return d[0]
+	}
+	return d
+}
+
+func (d Disjunction) Append(a Attrib) (Disjunction, error) {
+	switch a := a.(type) {
+	case Disjunction:
+		return append(d, a...), nil
+	case Expression:
+		return append(d, a.Simplify()), nil
+	}
+	return nil, fmt.Errorf("expected expression: %v", a)
+}
+
 type Negation struct {
 	Expression
+}
+
+func (n Negation) Simplify() Expression {
+	return n
 }
 
 func ParseText(a Attrib) (string, error) {
