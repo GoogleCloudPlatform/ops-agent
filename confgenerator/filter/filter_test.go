30acc4e4dc1927b4d79d5f14b6d0fffa6071cc71
Quentin Smith <quentin@google.com>
2021-10-29 19:29:43 -0400

    Add test for fb config

--- confgenerator/filter/filter_test.go
+++ confgenerator/filter/filter_test.go
@@ -18,8 +18,8 @@ import (
 	"testing"
 
 	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/filter/internal/lexer"
-	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/filter/internal/parser"
 	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/filter/internal/token"
+	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/fluentbit"
 )
 
 var validFilters = []string{
@@ -28,8 +28,8 @@ var validFilters = []string{
 	`foo = "hello"`,
 	`foo."bar.baz" = "hello"`,
 	`a.b.c=~"b.*c"`,
-	`-a < 1`,
-	`NOT a > 3`,
+	`-a = 1`,
+	`NOT a = 3`,
 	`(foo.bar = "one" OR foo.bar = "two") foo.baz = "three"`,
 	`foo.one = 1 foo.two = 2 AND foo.three = 3`,
 	`int_field:0 OR int_field:0 AND int_field:0`,
@@ -55,30 +55,17 @@ func TestShouldParse(t *testing.T) {
 	for _, test := range validFilters {
 		test := test
 		t.Run(test, func(t *testing.T) {
-			lex := lexer.NewLexer([]byte(test))
-			p := parser.NewParser()
-			st, err := p.Parse(lex)
+			filter, err := NewFilter(test)
 			if err != nil {
-				t.Fatal(err)
+				t.Error(err)
 			}
-			t.Logf("%#v", st)
+			components := filter.Components("logname")
+			t.Logf("components = %+v", components)
+			main, _, err := fluentbit.ModularConfig{Components: components}.Generate()
+			if err != nil {
+				t.Error(err)
+			}
+			t.Logf("generated config:\n%s", main)
 		})
 	}
 }
-
-// func TestLexerBasic(t *testing.T) {
-// 	l, err := lex.LexString("", `"this is a simple quoted string"`)
-// 	if err != nil {
-// 		t.Errorf("LexString: %v", err)
-// 	}
-// 	for {
-// 		tok, err := l.Next()
-// 		if err == io.EOF {
-// 			return
-// 		}
-// 		if err != nil {
-// 			t.Fatalf("Next: %v", err)
-// 		}
-// 		t.Logf("tok: %v", tok.GoString())
-// 	}
-// }
