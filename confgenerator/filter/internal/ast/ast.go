3514f3cf686ce0925d643732ad0fc757e1bf693c
Jeff Erbrecht <jefferbrecht@google.com>
2021-11-12 14:13:58 -0500

    Work around FB bug

--- confgenerator/filter/internal/ast/ast.go
+++ confgenerator/filter/internal/ast/ast.go
@@ -182,12 +182,16 @@ func (r Restriction) Components(tag, key string) []fluentbit.Component {
 		// regex match
 		c.Config["Condition"] = cond("Key_value_matches", lhs, rhsRegex)
 	case "!~":
-		c.Config["Condition"] = cond("Key_value_does_not_match", lhs, rhsRegex)
+		c.OrderedConfig = append(c.OrderedConfig, [2]string{"Condition", cond("Key_value_does_not_match", lhs, rhsRegex)})
+		// TODO: Remove when https://github.com/fluent/fluent-bit/issues/4319 is resolved.
+		c.OrderedConfig = append(c.OrderedConfig, [2]string{"Condition", cond("Key_exists", lhs)})
 	case "=":
 		// equality
 		c.Config["Condition"] = cond("Key_value_matches", lhs, fmt.Sprintf(`(?i)^%s$`, rhsLiteral))
 	case "!=":
-		c.Config["Condition"] = cond("Key_value_does_not_match", lhs, fmt.Sprintf(`(?i)^%s$`, rhsLiteral))
+		c.OrderedConfig = append(c.OrderedConfig, [2]string{"Condition", cond("Key_value_does_not_match", lhs, fmt.Sprintf(`(?i)^%s$`, rhsLiteral))})
+		// TODO: Remove when https://github.com/fluent/fluent-bit/issues/4319 is resolved.
+		c.OrderedConfig = append(c.OrderedConfig, [2]string{"Condition", cond("Key_exists", lhs)})
 	}
 	return []fluentbit.Component{c}
 }
