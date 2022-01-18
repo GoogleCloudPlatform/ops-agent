722507c9e6d9da257007dd91d965d4b1c259cf5d
schmikei <keith.schmitt@bluemedora.com>
2022-01-18 15:09:18 -0500

    revert unrelated change

--- confgenerator/confgenerator.go
+++ confgenerator/confgenerator.go
@@ -130,7 +130,7 @@ func (l *Logging) generateFluentbitComponents(userAgent string, hostInfo *host.I
 	}
 	out = append(out, fluentbit.Service{LogLevel: l.Service.LogLevel}.Component())
 
-	if l.Service != nil {
+	if l != nil && l.Service != nil {
 		// Type for sorting.
 		type fbSource struct {
 			tag        string
