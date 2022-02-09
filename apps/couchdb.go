06c948633a1b64ba3a5fb57e3c971c3860e850fa
Jonathan Wamsley <jonathan.wamsley@bluemedora.com>
2022-02-08 21:46:57 -0500

    add couchdb log parser

--- apps/couchdb.go
+++ apps/couchdb.go
@@ -16,6 +16,7 @@ package apps
 
 import (
 	"github.com/GoogleCloudPlatform/ops-agent/confgenerator"
+	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/fluentbit"
 	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/otel"
 )
 
@@ -60,4 +61,110 @@ func (r MetricsReceiverCouchdb) Pipelines() []otel.Pipeline {
 
 func init() {
 	confgenerator.MetricsReceiverTypes.RegisterType(func() confgenerator.Component { return &MetricsReceiverCouchdb{} })
+}
+
+type LoggingProcessorCouchdbError struct {
+	confgenerator.ConfigComponent `yaml:",inline"`
+}
+
+func (LoggingProcessorCouchdbError) Type() string {
+	return "couchdb_error"
+}
+
+func (p LoggingProcessorCouchdbError) Components(tag string, uid string) []fluentbit.Component {
+	c := confgenerator.LoggingProcessorParseMultilineRegex{
+		LoggingProcessorParseRegexComplex: confgenerator.LoggingProcessorParseRegexComplex{
+			Parsers: []confgenerator.RegexParser{
+				{
+					// Format https://github.com/apache/couchdb/blob/main/src/couch_log/src/couch_log_writer_syslog.erl#L72
+					// Sample line: [notice] 2021-12-02T23:36:42.555157Z nonode@nohost <0.17165.1> a5f585a0d3 localhost:5984 127.0.0.1 otelu PUT /oteld 201 ok 16
+					Regex: `^\[(?<severity>\w*)\] (?<timestamp>[\d\-\.:TZ]+) (?<user>\w+)@(?<host>[^\s]+) \<(?<process_id>[^ ]*)\> [\w-]+ ([^ ]*) (?<remote_host>[^ ]*) (?<remote_user>[^ ]*) (?<method>[^ ]*) (?<path>[^ ]*) (?<status>[^ ]*) (?<message>[^ ]*) (?<bytes_sent>[\d]*)$`,
+					Parser: confgenerator.ParserShared{
+						TimeKey:    "timestamp",
+						TimeFormat: "%Y-%m-%dTH:%M:%S.%L%z",
+						Types: map[string]string{
+							"status":     "integer",
+							"bytes_sent": "integer",
+						},
+					},
+				},
+				{
+					// Format https://github.com/apache/couchdb/blob/main/src/couch_log/src/couch_log_writer_syslog.erl#L72
+					/* Sample line:
+					[error] 2022-01-12T16:53:03.094488Z nonode@nohost emulator -------- Error in process <0.463.0> with exit value:
+					{database_does_not_exist,[{mem3_shards,load_shards_from_db,"_users",[{file,"src/mem3_shards.erl"},{line,399}]},{mem3_shards,load_shards_from_disk,1,[{file,"src/mem3_shards.erl"},{line,374}]},{mem3_shards,load_shards_from_disk,2,[{file,"src/mem3_shards.erl"},{line,403}]},{mem3_shards,for_docid,3,[{file,"src/mem3_shards.erl"},{line,96}]},{fabric_doc_open,go,3,[{file,"src/fabric_doc_open.erl"},{line,39}]},{chttpd_auth_cache,ensure_auth_ddoc_exists,2,[{file,"src/chttpd_auth_cache.erl"},{line,198}]},{chttpd_auth_cache,listen_for_changes,1,[{file,"src/chttpd_auth_cache.erl"},{line,145}]}]}
+					*/
+					Regex: `^\[(?<severity>\w*)\] (?<timestamp>[\d\-\.:TZ]+) (?<user>\w+)@(?<host>[^\s]+) (\w+) [\w-]+ (?<message>[\s\S]*)`,
+					Parser: confgenerator.ParserShared{
+						TimeKey:    "timestamp",
+						TimeFormat: "%Y-%m-%dTH:%M:%S.%L%z",
+					},
+				},
+				{
+					// Format https://github.com/apache/couchdb/blob/main/src/couch_log/src/couch_log_writer_syslog.erl#L72
+					// Sample line: [info] 2022-01-12T16:52:56.998128Z nonode@nohost <0.216.0> -------- Apache CouchDB has started. Time to relax.
+					Regex: `^\[(?<severity>\w*)\] (?<timestamp>[\d\-\.:TZ]+) (?<user>\w+)@(?<host>[^\s]+) \<(?<process_id>[^>]+)\> [\w-]+ (?<message>.*)`,
+					Parser: confgenerator.ParserShared{
+						TimeKey:    "timestamp",
+						TimeFormat: "%Y-%m-%dTH:%M:%S.%L%z",
+					},
+				},
+			},
+		},
+		Rules: []confgenerator.MultilineRule{
+			{
+				StateName: "start_state",
+				NextState: "cont",
+				Regex:     `^\[\w+\]`,
+			},
+			{
+				StateName: "cont",
+				NextState: "cont",
+				Regex:     `^\[\w+\]`,
+			},
+		},
+	}.Components(tag, uid)
+
+	// Log levels documented: https://docs.couchdb.org/en/stable/config/logging.html#log/level
+	c = append(c,
+		fluentbit.TranslationComponents(tag, "level", "logging.googleapis.com/severity", false,
+			[]struct{ SrcVal, DestVal string }{
+				{"emerg", "EMERGENCY"},
+				{"emergency", "EMERGENCY"},
+				{"alert", "ALERT"},
+				{"crit", "CRITICAL"},
+				{"critical", "CRITICAL"},
+				{"error", "ERROR"},
+				{"err", "ERROR"},
+				{"warn", "WARNING"},
+				{"warning", "WARNING"},
+				{"notice", "NOTICE"},
+				{"info", "INFO"},
+				{"debug", "DEBUG"},
+			},
+		)...,
+	)
+	return c
+}
+
+type LoggingReceiverCouchdbError struct {
+	LoggingProcessorCouchdbError            `yaml:",inline"`
+	confgenerator.LoggingReceiverFilesMixin `yaml:",inline" validate:"structonly"`
+}
+
+func (r LoggingReceiverCouchdbError) Components(tag string) []fluentbit.Component {
+	if len(r.IncludePaths) == 0 {
+		r.IncludePaths = []string{
+			// Default log file
+			"/var/log/couchdb/couchdb.log",
+		}
+	}
+	c := r.LoggingReceiverFilesMixin.Components(tag)
+	c = append(c, r.LoggingProcessorCouchdbError.Components(tag, "couchdb_error")...)
+	return c
+}
+
+func init() {
+	confgenerator.LoggingProcessorTypes.RegisterType(func() confgenerator.Component { return &LoggingProcessorCouchdbError{} })
+	confgenerator.LoggingReceiverTypes.RegisterType(func() confgenerator.Component { return &LoggingReceiverCouchdbError{} })
 }
