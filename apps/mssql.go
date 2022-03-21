65c98dc44f3df118b3ca7a5d0a6f3e54892bcb5a
Phil Brammer <phil.brammer@gmail.com>
2022-03-21 16:22:45 -0500

    Added in support for MS SQL Server error log

--- apps/mssql.go
+++ apps/mssql.go
@@ -15,61 +15,62 @@
 package apps
 
 import (
-	"github.com/GoogleCloudPlatform/ops-agent/confgenerator"
-	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/otel"
+        "github.com/GoogleCloudPlatform/ops-agent/confgenerator"
+        "github.com/GoogleCloudPlatform/ops-agent/confgenerator/otel"
+        "github.com/GoogleCloudPlatform/ops-agent/confgenerator/fluentbit"
 )
 
 type MetricsReceiverMssql struct {
-	confgenerator.ConfigComponent `yaml:",inline"`
+        confgenerator.ConfigComponent `yaml:",inline"`
 
-	confgenerator.MetricsReceiverShared `yaml:",inline"`
-}
+        confgenerator.MetricsReceiverShared `yaml:",inline"`
+} 
 
 func (MetricsReceiverMssql) Type() string {
-	return "mssql"
+        return "mssql"
 }
 
 func (m MetricsReceiverMssql) Pipelines() []otel.Pipeline {
-	return []otel.Pipeline{{
-		Receiver: otel.Component{
-			Type: "windowsperfcounters",
-			Config: map[string]interface{}{
-				"collection_interval": m.CollectionIntervalString(),
-				"perfcounters": []map[string]interface{}{
-					{
-						"object":    "SQLServer:General Statistics",
-						"instances": []string{"_Total"},
-						"counters":  []string{"User Connections"},
-					},
-					{
-						"object":    "SQLServer:Databases",
-						"instances": []string{"_Total"},
-						"counters": []string{
-							"Transactions/sec",
-							"Write Transactions/sec",
-						},
-					},
-				},
-			},
-		},
-		Processors: []otel.Component{
-			otel.MetricsTransform(
-				otel.RenameMetric(
-					`\SQLServer:General Statistics(_Total)\User Connections`,
-					"mssql/connections/user",
-				),
-				otel.RenameMetric(
-					`\SQLServer:Databases(_Total)\Transactions/sec`,
-					"mssql/transaction_rate",
-				),
-				otel.RenameMetric(
-					`\SQLServer:Databases(_Total)\Write Transactions/sec`,
-					"mssql/write_transaction_rate",
-				),
-				otel.AddPrefix("agent.googleapis.com"),
-			),
-		},
-	}}
+        return []otel.Pipeline{{
+                Receiver: otel.Component{
+                        Type: "windowsperfcounters",
+                        Config: map[string]interface{}{
+                                "collection_interval": m.CollectionIntervalString(),
+                                "perfcounters": []map[string]interface{}{
+                                        {
+                                                "object":    "SQLServer:General Statistics",
+                                                "instances": []string{"_Total"},
+                                                "counters":  []string{"User Connections"},
+                                        },
+                                        {
+                                                "object":    "SQLServer:Databases",
+                                                "instances": []string{"_Total"},
+                                                "counters": []string{
+                                                        "Transactions/sec",
+                                                        "Write Transactions/sec",
+                                                },
+                                        },
+                                },
+                        },
+                },
+                Processors: []otel.Component{
+                        otel.MetricsTransform(
+                                otel.RenameMetric(
+                                        `\SQLServer:General Statistics(_Total)\User Connections`,
+                                        "mssql/connections/user",
+                                ),
+                                otel.RenameMetric(
+                                        `\SQLServer:Databases(_Total)\Transactions/sec`,
+                                        "mssql/transaction_rate",
+                                ),
+                                otel.RenameMetric(
+                                        `\SQLServer:Databases(_Total)\Write Transactions/sec`,
+                                        "mssql/write_transaction_rate",
+                                ),
+                                otel.AddPrefix("agent.googleapis.com"),
+                        ),
+                },
+        }}
 }
 
 type LoggingProcessorMssqlLog struct {
@@ -85,13 +86,25 @@ func (p LoggingProcessorMssqlLog) Components(tag string, uid string) []fluentbit
                 LoggingProcessorParseRegexComplex: confgenerator.LoggingProcessorParseRegexComplex{
                         Parsers: []confgenerator.RegexParser{
                                 {
-                                        Regex: `^(?<logtime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{2}) (?<spid>.{11}) (?<message>[\s|\S]*)?`,
-                                        //Regex: `^(?<logtime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{2}) (?<spid>.{11}) (?<message>.*)?`,
-                                        Parser: confgenerator.ParserShared{
-                                                Types: map[string]string{
-                                                        "spid": "integer",
-                                                },
-                                        },
+					// SAMPLE LOG ENTRIES, including multiline:
+					//
+					// 2022-03-20 00:00:00.27 spid56      Microsoft SQL Server 2019 (RTM-CU4) (KB4548597) - 15.0.4033.1 (X64) 
+					// 	Mar 14 2020 16:10:35 
+					// 	Copyright (C) 2019 Microsoft Corporation
+					// 	Developer Edition (64-bit) on Windows Server 2019 Datacenter 10.0 <X64> (Build 17763: ) (Hypervisor)
+					// 
+					// 2022-03-20 00:00:00.28 spid56      UTC adjustment: -5:00
+					// 2022-03-20 00:00:00.28 spid56      (c) Microsoft Corporation.
+					// 2022-03-20 00:00:00.28 spid56      All rights reserved.
+					// 2022-03-20 00:00:00.28 spid56      Server process ID is 3432.
+					// 2022-03-20 00:00:00.28 spid56      System Manufacturer: 'Google', System Model: 'Google Compute Engine'.
+					// 2022-03-20 00:00:00.28 spid56      Authentication mode is MIXED.
+					// 2022-03-20 00:00:01.90 Backup      Log was backed up. Database: demo, creation date(time): 2020/01/31(10:33:17), first LSN: 582441:259880:1, last LSN: 582441:259912:1, number of dump devices: 1, device information: (FILE=1, TYPE=DISK: {'\\server\share\DatabaseBackups\demo.trn'}). This is an informational message only. No user action is required.
+					// 2022-03-20 00:00:03.76 Logon       Error: 18456, Severity: 14, State: 38.
+					//
+                                        Regex: `^(?<logtime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{2}) (?<spid>\w+)\s+(?<message>[\s|\S]*)?`,
+					// Not sending the log timestamp "logtime" from above in as the Time_Key as there will be time zone issues.
+					// MS SQL Server writes logs in server time, not a standard time zone such as UTC
                                 },
                         },
                 },
@@ -109,6 +122,18 @@ func (p LoggingProcessorMssqlLog) Components(tag string, uid string) []fluentbit
                 },
         }.Components(tag, uid)
 
+	// set default log level to info.  Couldn't get this to work via config.yaml
+        comp := fluentbit.Component{
+                        Kind: "FILTER",
+                        Config: map[string]string{
+                                "Name":      "modify",
+                                "Match":     tag,
+                                "Add":       "logging.googleapis.com/severity info",
+                        },
+                }
+
+        c = append(c, comp)
+	
         return c
 }
 
@@ -131,7 +156,7 @@ func (r LoggingReceiverMssqlLog) Components(tag string) []fluentbit.Component {
 }
 
 func init() {
-	confgenerator.MetricsReceiverTypes.RegisterType(func() confgenerator.Component { return &MetricsReceiverMssql{} }, "windows")
-	confgenerator.LoggingProcessorTypes.RegisterType(func() confgenerator.Component { return &LoggingProcessorMssqlLog{} })
+        confgenerator.MetricsReceiverTypes.RegisterType(func() confgenerator.Component { return &MetricsReceiverMssql{} }, "windows")
+        confgenerator.LoggingProcessorTypes.RegisterType(func() confgenerator.Component { return &LoggingProcessorMssqlLog{} })
         confgenerator.LoggingReceiverTypes.RegisterType(func() confgenerator.Component { return &LoggingReceiverMssqlLog{} })
 }
