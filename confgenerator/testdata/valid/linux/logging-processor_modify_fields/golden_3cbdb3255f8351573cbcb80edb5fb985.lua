2181a74d36e71da74686e5ec7dd36ad725bc47bb
Quentin Smith <quentin@google.com>
2022-03-22 15:05:56 -0400

    Add unit test for modify_fields config

---
+++ confgenerator/testdata/valid/linux/logging-processor_modify_fields/golden_3cbdb3255f8351573cbcb80edb5fb985.lua
@@ -0,0 +1,62 @@
+
+function process(tag, timestamp, record)
+local __field_0 = (function()
+return record["source"]
+end)();
+local __field_1 = (function()
+return record["integer"]
+end)();
+local __field_2 = (function()
+return record["move_source"]
+end)();
+local __field_3 = (function()
+return record["unnested"]
+end)();
+local __field_4 = (function()
+return record["level"]
+end)();
+(function(value)
+record["move_source"] = value
+end)(nil)
+local v = __field_0;
+(function(value)
+record["copied"] = value
+end)(v)
+local v = "world";
+(function(value)
+record["hello"] = value
+end)(v)
+local v = __field_1;
+
+local v2 = math.tointeger(v)
+if v2 != fail then v = v2
+end
+(function(value)
+record["integer"] = value
+end)(v)
+local v = __field_2;
+(function(value)
+record["moved"] = value
+end)(v)
+local v = __field_3;
+(function(value)
+if record["nested"] == nil
+then
+record["nested"] = {}
+end
+if record["nested"]["structure"] == nil
+then
+record["nested"]["structure"] = {}
+end
+record["nested"]["structure"]["field"] = value
+end)(v)
+local v = __field_4;
+if v == "CAUTION" then v = "WARNING"
+elseif v == "I" then v = "INFO"
+elseif v == "W" then v = "WARNING"
+end
+(function(value)
+record["logging.googleapis.com/severity"] = value
+end)(v)
+return 2, timestamp, record
+end
