c9118cf3d670587758e6e2f60a31ecf3f4da7fec
Mrod1598 <miguel.rodriguez@observiq.com>
2022-04-05 19:19:28 -0400

    Fix goldens

---
+++ confgenerator/testdata/valid/windows/logging-receiver_iis/golden_1cc19db070474ee89de487e76219cc35.lua
@@ -0,0 +1,11 @@
+
+	function iis_merge_fields(tag, timestamp, record)
+	  record["http_request_serverIp"] = table.concat({record["http_request_serverIp"], ":", record["s_port"]})
+	  if (record["cs_uri_query"] == nil or record["cs_uri_query"] == '') then
+		record["http_request_requestUrl"] = record["cs_uri_stem"]
+	  else
+		record["http_request_requestUrl"] = table.concat({record["cs_uri_stem"], "?", record["cs_uri_query"]})
+	  end
+	  return 2, timestamp, record
+	end
+	
