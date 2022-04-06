cfe14bf2fa2346b8f7260b4b26b6f877f1084516
Jeff Erbrecht <jefferbrecht@google.com>
2022-04-06 16:59:04 -0400

    Replace manual validation code with validator

--- integration_test/common/common.go
+++ integration_test/common/common.go
@@ -2,8 +2,8 @@ package common
 
 import (
 	"fmt"
-	"strings"
 
+	"github.com/go-playground/validator/v10"
 	"go.uber.org/multierr"
 )
 
@@ -13,47 +13,29 @@ type ExpectedMetric struct {
 	// The metric type, for example workload.googleapis.com/apache.current_connections.
 	Type string `yaml:"type"`
 	// The value type, for example INT64.
-	ValueType string `yaml:"value_type"`
+	ValueType string `yaml:"value_type" validate:"oneof=BOOL INT64 DOUBLE STRING DISTRIBUTION"`
 	// The kind, for example GAUGE.
-	Kind string `yaml:"kind"`
+	Kind string `yaml:"kind" validate:"oneof=GAUGE DELTA CUMULATIVE"`
 	// The monitored resource, for example gce_instance.
-	// Currently we only test with gce_instance, so we expect
-	// all expectedMetricsEntries to have gce_instance.
-	MonitoredResource string `yaml:"monitored_resource"`
+	// Currently we only test with gce_instance.
+	MonitoredResource string `yaml:"monitored_resource" validate:"oneof=gce_instance"`
 	// Mapping of expected label keys to value patterns.
 	// Patterns are RE2 regular expressions.
 	Labels map[string]string `yaml:"labels"`
 	// If Optional is true, the test for this metric will be skipped.
-	Optional bool `yaml:"optional,omitempty"`
+	Optional bool `yaml:"optional,omitempty" validate:"necsfield=Representative"`
 	// Exactly one metric in each expected_metrics.yaml must
 	// have Representative set to true. This metric can be used
 	// to test that the integration is enabled.
-	Representative bool `yaml:"representative,omitempty"`
+	Representative bool `yaml:"representative,omitempty" validate:"necsfield=Optional"`
 }
 
 // ValidateMetrics checks that all enum fields have valid values and that
 // there is exactly one representative metric in the slice.
 func ValidateMetrics(metrics []ExpectedMetric) error {
-	var err error
 	// Field validation
-	expectedKinds := []string{"GAUGE", "DELTA", "CUMULATIVE"}
-	expectedValueTypes := []string{"BOOL", "INT64", "DOUBLE", "STRING", "DISTRIBUTION"}
-	expectedResource := "gce_instance"
-	for _, metric := range metrics {
-		innerErrs := make([]string, 0)
-		if !SliceContains(expectedKinds, metric.Kind) {
-			innerErrs = append(innerErrs, fmt.Sprintf("invalid kind %s (must be %v)", metric.Kind, expectedKinds))
-		}
-		if !SliceContains(expectedValueTypes, metric.ValueType) {
-			innerErrs = append(innerErrs, fmt.Sprintf("invalid value_type %s (must be %v)", metric.ValueType, expectedValueTypes))
-		}
-		if expectedResource != metric.MonitoredResource {
-			innerErrs = append(innerErrs, fmt.Sprintf("invalid monitored_resource %s (must be %v)", metric.MonitoredResource, expectedResource))
-		}
-		if len(innerErrs) > 0 {
-			err = multierr.Append(err, fmt.Errorf("%s: %v", metric.Type, strings.Join(innerErrs, ", ")))
-		}
-	}
+	v := validator.Validate{}
+	err := v.Struct(metrics)
 	// Representative validation
 	representativeCount := 0
 	for _, metric := range metrics {
