de29d4b39a53f06edacec91671c6ed98e1cf448b
Ridwan Sharif <ridwanmsharif@google.com>
2022-04-07 00:53:31 +0000

    confgenerator: Update golden files to add log_file_path
    Signed-off-by: Ridwan Sharif <ridwanmsharif@google.com>

---
+++ confgenerator/testdata/valid/linux/logging-receiver_apache_custom/golden_a59317aa56ebc2556d3285abad7ce066.lua
@@ -0,0 +1,34 @@
+
+function process(tag, timestamp, record)
+local __field_0 = (function()
+return record["agent.googleapis.com/log_file_path"]
+end)();
+local __field_1 = (function()
+return record["logging.googleapis.com/logName"]
+end)();
+(function(value)
+record["agent.googleapis.com/log_file_path"] = value
+end)(nil)
+local v = __field_0;
+(function(value)
+if record["logging.googleapis.com/labels"] == nil
+then
+record["logging.googleapis.com/labels"] = {}
+end
+record["logging.googleapis.com/labels"]["agent.googleapis.com/log_file_path"] = value
+end)(v)
+local v = "apache_error";
+(function(value)
+if record["logging.googleapis.com/labels"] == nil
+then
+record["logging.googleapis.com/labels"] = {}
+end
+record["logging.googleapis.com/labels"]["agent.googleapis.com/receiver_type"] = value
+end)(v)
+local v = __field_1;
+if v == nil then v = "apache_default_error" end;
+(function(value)
+record["logging.googleapis.com/logName"] = value
+end)(v)
+return 2, timestamp, record
+end
