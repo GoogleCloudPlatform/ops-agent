6169fed90e41e88b513f4ef34465720bfb947208
braydonk <braydonk@google.com>
2022-04-09 01:19:51 +0000

    integration_test/logging: not build tagged
    This package doesn't need the `integration_test` build flag. It was
    added by mistake.

--- integration_test/logging/logging_test.go
+++ integration_test/logging/logging_test.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build integration_test
-
 // Unit tests for the DirectoryLogger, which is itself only used
 // for integration testing.
 
