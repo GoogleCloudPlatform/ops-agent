5cb97e2426bed06f598810873ec0390cf3ae1c2d
Jeff Erbrecht <jefferbrecht@google.com>
2022-04-14 17:56:24 -0400

    Force unit test re-run (flake)

--- confgenerator/confgenerator.go
+++ confgenerator/confgenerator.go
@@ -77,8 +77,8 @@ func (uc *UnifiedConfig) GenerateOtelConfig(hostInfo *host.InfoStat) (string, er
 					// NB: If a receiver fails to send a full URL, OT will add the prefix `workload.googleapis.com/{metric_name}`.
 					// TODO(b/197129428): Write a test to make sure this doesn't happen.
 					"prefix": "",
-					// OT calls CreateMetricDescriptor by default. Skip because we
-					// want descriptors to be created implicitly with new time series.
+					// OT calls CreateMetricDescriptor by default. Skip because we want
+					// descriptors to be created implicitly with new time series.
 					"skip_create_descriptor": true,
 					// Omit instrumentation labels, which break agent metrics.
 					"instrumentation_library_labels": false,
