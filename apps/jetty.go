fc0d9cbe5f3628dd7e32e871794cb0f16feee1b4
Miguel Rodriguez <miguel.rodriguez@observiq.com>
2022-05-17 11:52:37 -0400

    Add Jetty logging (#573)
    * Add jetty logging
    
    * Add to supported apps
    
    * Seperate mkdir
    
    * Goldens
    
    * Update metadata
    
    * Remove system log processor
    
    * Add syslog check
    
    * Fix message regex
    
    * Change java version
    
    * Replace null

---
+++ apps/jetty.go
@@ -0,0 +1,53 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package apps
+
+import (
+	"github.com/GoogleCloudPlatform/ops-agent/confgenerator"
+	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/fluentbit"
+)
+
+type LoggingProcessorJettyAccess struct {
+	confgenerator.ConfigComponent `yaml:",inline"`
+}
+
+func (p LoggingProcessorJettyAccess) Components(tag string, uid string) []fluentbit.Component {
+	return genericAccessLogParser(tag, uid)
+}
+
+func (LoggingProcessorJettyAccess) Type() string {
+	return "jetty_access"
+}
+
+type LoggingReceiverJettyAccess struct {
+	LoggingProcessorJettyAccess             `yaml:",inline"`
+	confgenerator.LoggingReceiverFilesMixin `yaml:",inline" validate:"structonly"`
+}
+
+func (r LoggingReceiverJettyAccess) Components(tag string) []fluentbit.Component {
+	if len(r.IncludePaths) == 0 {
+		r.IncludePaths = []string{
+			"/opt/logs/*.request.log",
+		}
+	}
+	c := r.LoggingReceiverFilesMixin.Components(tag)
+	c = append(c, r.LoggingProcessorJettyAccess.Components(tag, "jetty_access")...)
+	return c
+}
+
+func init() {
+	confgenerator.LoggingProcessorTypes.RegisterType(func() confgenerator.Component { return &LoggingProcessorJettyAccess{} })
+	confgenerator.LoggingReceiverTypes.RegisterType(func() confgenerator.Component { return &LoggingReceiverJettyAccess{} })
+}
