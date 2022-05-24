27a7b232c9a02d51584d9af5efe93a311180ab71
Quentin Smith <quentin@google.com>
2022-05-24 17:56:02 -0400

    update_golden

--- confgenerator/testdata/valid/linux/logging-receiver_jetty/golden_db0271053700ddd265b8e8405af3d6d9.lua
+++ confgenerator/testdata/valid/linux/logging-receiver_jetty/golden_db0271053700ddd265b8e8405af3d6d9.lua
@@ -31,7 +31,7 @@ local __field_9 = (function()
 return record["user"]
 end)();
 local omit1 = (function(v) if v == nil then return false end return (string.lower(tostring(v)) == string.lower("-")) end)((function()
-return record["http_request_%s"]
+return record["http_request_referer"]
 end)());
 local omit8 = (function(v) if v == nil then return false end return (string.lower(tostring(v)) == string.lower("-")) end)((function()
 return record["host"]
@@ -137,6 +137,14 @@ local v = __field_9;
 if omit9 then v = nil end;
 (function(value)
 record["user"] = value
+end)(v)
+local v = "agent.googleapis.com/jetty_access";
+(function(value)
+if record["logging.googleapis.com/labels"] == nil
+then
+record["logging.googleapis.com/labels"] = {}
+end
+record["logging.googleapis.com/labels"]["logging.googleapis.com/instrumentation_source"] = value
 end)(v)
 return 2, timestamp, record
 end
--- confgenerator/testdata/valid/linux/logging-receiver_nginx_custom/golden_6b0505abd8b2ee200942f614682da48e.lua
+++ confgenerator/testdata/valid/linux/logging-receiver_nginx_custom/golden_6b0505abd8b2ee200942f614682da48e.lua
@@ -138,5 +138,13 @@ if omit9 then v = nil end;
 (function(value)
 record["user"] = value
 end)(v)
+local v = "agent.googleapis.com/nginx_access";
+(function(value)
+if record["logging.googleapis.com/labels"] == nil
+then
+record["logging.googleapis.com/labels"] = {}
+end
+record["logging.googleapis.com/labels"]["logging.googleapis.com/instrumentation_source"] = value
+end)(v)
 return 2, timestamp, record
 end
