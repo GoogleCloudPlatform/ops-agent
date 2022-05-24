b0d0b4b3596bf96152fc6e042f47247981674428
Quentin Smith <quentin@google.com>
2022-05-24 18:02:44 -0400

    update_golden

--- confgenerator/testdata/valid/linux/logging-receiver_cassandra_custom/golden_0602f2481c678c2fbe8dcedde0a7ce1e.lua
+++ confgenerator/testdata/valid/linux/logging-receiver_cassandra_custom/golden_0602f2481c678c2fbe8dcedde0a7ce1e.lua
@@ -1,9 +1,17 @@
 
 function process(tag, timestamp, record)
-local __field_0 = (function()
+local __field_1 = (function()
 return record["level"]
 end)();
-local v = __field_0;
+local v = "agent.googleapis.com/cassandra_system";
+(function(value)
+if record["logging.googleapis.com/labels"] == nil
+then
+record["logging.googleapis.com/labels"] = {}
+end
+record["logging.googleapis.com/labels"]["logging.googleapis.com/instrumentation_source"] = value
+end)(v)
+local v = __field_1;
 if v == "DEBUG" then v = "DEBUG"
 elseif v == "ERROR" then v = "ERROR"
 elseif v == "INFO" then v = "INFO"
