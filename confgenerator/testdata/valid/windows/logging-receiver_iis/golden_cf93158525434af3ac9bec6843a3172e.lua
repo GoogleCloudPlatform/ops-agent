0d9e9e99eff534311b52e3b74d854c38d0f2f4eb
Francisco Valente <fcovalente@google.com>
2022-06-01 13:00:05 +0000

    Update goldens.

---
+++ confgenerator/testdata/valid/windows/logging-receiver_iis/golden_cf93158525434af3ac9bec6843a3172e.lua
@@ -0,0 +1,82 @@
+
+function process(tag, timestamp, record)
+local __field_0 = (function()
+return record["http_request_remoteIp"]
+end)();
+local __field_1 = (function()
+return record["http_request_requestMethod"]
+end)();
+local __field_2 = (function()
+return record["http_request_responseSize"]
+end)();
+local __field_3 = (function()
+return record["http_request_serverIp"]
+end)();
+local __field_4 = (function()
+return record["http_request_status"]
+end)();
+(function(value)
+record["http_request_remoteIp"] = value
+end)(nil);
+(function(value)
+record["http_request_requestMethod"] = value
+end)(nil);
+(function(value)
+record["http_request_responseSize"] = value
+end)(nil);
+(function(value)
+record["http_request_serverIp"] = value
+end)(nil);
+(function(value)
+record["http_request_status"] = value
+end)(nil);
+local v = __field_0;
+(function(value)
+if record["logging.googleapis.com/http_request"] == nil
+then
+record["logging.googleapis.com/http_request"] = {}
+end
+record["logging.googleapis.com/http_request"]["remoteIp"] = value
+end)(v)
+local v = __field_1;
+(function(value)
+if record["logging.googleapis.com/http_request"] == nil
+then
+record["logging.googleapis.com/http_request"] = {}
+end
+record["logging.googleapis.com/http_request"]["requestMethod"] = value
+end)(v)
+local v = __field_2;
+(function(value)
+if record["logging.googleapis.com/http_request"] == nil
+then
+record["logging.googleapis.com/http_request"] = {}
+end
+record["logging.googleapis.com/http_request"]["responseSize"] = value
+end)(v)
+local v = __field_3;
+(function(value)
+if record["logging.googleapis.com/http_request"] == nil
+then
+record["logging.googleapis.com/http_request"] = {}
+end
+record["logging.googleapis.com/http_request"]["serverIp"] = value
+end)(v)
+local v = __field_4;
+(function(value)
+if record["logging.googleapis.com/http_request"] == nil
+then
+record["logging.googleapis.com/http_request"] = {}
+end
+record["logging.googleapis.com/http_request"]["status"] = value
+end)(v)
+local v = "agent.googleapis.com/iis_access";
+(function(value)
+if record["logging.googleapis.com/labels"] == nil
+then
+record["logging.googleapis.com/labels"] = {}
+end
+record["logging.googleapis.com/labels"]["logging.googleapis.com/instrumentation_source"] = value
+end)(v)
+return 2, timestamp, record
+end
