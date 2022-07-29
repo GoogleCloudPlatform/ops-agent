03d36a5f87be0f578bed38bfb4cd3328f292636f
Martijn van Schaardenburg <martijnvs@google.com>
2022-07-29 12:22:25 -0400

    undo dev changes, add comment

--- kokoro/scripts/build/build_package.bat
+++ kokoro/scripts/build/build_package.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
-echo hello
-echo %~dp0
-echo %~dp0\build_package.ps1
+REM %~dp0 means "the directory this script is stored in, with a trailing backslash".
+REM So overall, this command means "execute subling file build_package.ps1".
+PowerShell.exe -Command "& %~dp0build_package.ps1"
+
+exit %ERRORLEVEL%
