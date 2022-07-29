c0268aa42422a8a46b8d4509235fb70b4fa34659
Martijn van Schaardenburg <martijnvs@google.com>
2022-07-29 12:29:41 -0400

    subling->sibling

--- kokoro/scripts/build/build_package.bat
+++ kokoro/scripts/build/build_package.bat
@@ -2,7 +2,7 @@
 
 REM %~dp0 means "the directory this script is stored in, with a trailing backslash".
 REM https://stackoverflow.com/a/112135
-REM So overall, this command means "execute subling file build_package.ps1".
+REM So overall, this command means "execute sibling file build_package.ps1".
 PowerShell.exe -Command "& %~dp0build_package.ps1"
 
 exit %ERRORLEVEL%
