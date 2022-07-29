36c4adb8ede62f589a48bbee435443fe6db484c9
Martijn van Schaardenburg <martijnvs@google.com>
2022-07-29 12:25:36 -0400

    Add citation for %~dp0

--- kokoro/scripts/build/build_package.bat
+++ kokoro/scripts/build/build_package.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 REM %~dp0 means "the directory this script is stored in, with a trailing backslash".
+REM https://stackoverflow.com/a/112135
 REM So overall, this command means "execute subling file build_package.ps1".
 PowerShell.exe -Command "& %~dp0build_package.ps1"
 
