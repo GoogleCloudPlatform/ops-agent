3071f355a8676e4038c2fb4cbe818bc8c3efbf55
Lujie Duan <lujieduan@google.com>
2022-09-29 23:34:52 +0000

    Add comment to the setup script

--- integration_test/testdata/setup_json_exporter.sh
+++ integration_test/testdata/setup_json_exporter.sh
@@ -1,10 +1,16 @@
 set -e
 
+# Download JSON exporter and extract to WORKDIR
 wget -O json_exporter.tar.gz \
     https://github.com/prometheus-community/json_exporter/releases/download/v0.5.0/json_exporter-0.5.0.linux-amd64.tar.gz
 tar -xvf json_exporter.tar.gz -C $WORKDIR/
 
-nohup bash -c "cd $WORKDIR && python3 -u -m http.server 8000" >python-server.log 2>python-server.err </dev/null &
-echo "Python server started."
-nohup $WORKDIR/json_exporter-0.5.0.linux-amd64/json_exporter --config.file $WORKDIR/json_exporter_config.yaml >json-exporter.log 2>json-exporter.err </dev/null & 
-echo "JSON exporter started."
+# Start a python http server in the WORKDIR
+# In order to have the server runs in the background, redirect all stdin, stdout and sederr
+nohup bash -c "cd $WORKDIR && python3 -u -m http.server 8000" \
+    >python-server.log 2>python-server.err </dev/null &
+
+# Start the JSON exporter with uploaded config yaml
+nohup $WORKDIR/json_exporter-0.5.0.linux-amd64/json_exporter \
+    --config.file $WORKDIR/json_exporter_config.yaml \
+    >json-exporter.log 2>json-exporter.err </dev/null & 
