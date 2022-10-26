5243fe2797731045bc97e1bb68ae56907a987bfb
Jeff Erbrecht <89024676+jefferbrecht@users.noreply.github.com>
2022-10-26 16:37:35 -0400

    Fix typo (#927)
    * Fix typo
    
    * Fix typo

--- integration_test/third_party_apps_test.go
+++ integration_test/third_party_apps_test.go
@@ -228,7 +228,7 @@ func verifyLogField(fieldName, actualField string, expectedFields map[string]*me
 	expectedField, ok := expectedFields[fieldName]
 	if !ok {
 		// Not expecting this field. It could however be populated with some default zero-values when we
-		// query it back. Check for zero values basued on expectedField.type? Not ideal for sure.
+		// query it back. Check for zero values based on expectedField.type? Not ideal for sure.
 		if actualField != "" && actualField != "0" && actualField != "false" && actualField != "0s" {
 			return fmt.Errorf("expeced no value for field %s but got %v\n", fieldName, actualField)
 		}
