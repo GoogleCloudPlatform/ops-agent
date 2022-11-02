3535635674bb3f15f7e4878cf2d014f3e9d3e948
Martijn van Schaardenburg <martijnvs@google.com>
2022-11-02 15:49:19 -0400

    try with & instead of invoke-program

--- kokoro/scripts/build/build_package.ps1
+++ kokoro/scripts/build/build_package.ps1
@@ -49,9 +49,15 @@ PACKAGE_VERSION,$env:PKG_VERSION
 Invoke-Program git submodule update --init
 $artifact_registry="us-docker.pkg.dev"
 Invoke-Program gcloud version
-Invoke-Program gcloud components update
+& gcloud components update
 Invoke-Program gcloud version
-Invoke-Program gcloud auth 'configure-docker' 'us-docker.pkg.dev'
+& gcloud auth 'configure-docker' 'us-docker.pkg.dev'
+
+if ( $LastExitCode -ne 0 ) {
+  throw "gcloud failed"
+}
+
+Asdf
 
 $cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows"
 Invoke-Program docker build -t $tag -f './Dockerfile.windows' .
