004c2a24a2ee4c50604ff43d6a32468822d4729d
Martijn van Schaardenburg <martijnvs@google.com>
2022-11-03 17:16:02 -0400

    remove explicit docker pull: do we need it or is cache-from enough?

--- kokoro/scripts/build/build_package.ps1
+++ kokoro/scripts/build/build_package.ps1
@@ -54,9 +54,6 @@ Invoke-Program gcloud version
 & gcloud auth configure-docker $artifact_registry
 
 $cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows"
-
-docker pull $cache_location
-
 Invoke-Program docker build --cache-from="${cache_location}" -t $tag -f './Dockerfile.windows' .
 Invoke-Program docker create --name $name $tag
 Invoke-Program docker cp "${name}:/work/out" $env:KOKORO_ARTIFACTS_DIR
