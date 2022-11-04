05c5cf69b007df9e76a628c68c220861dc7be31b
Martijn van Schaardenburg <martijnvs@google.com>
2022-11-04 10:36:22 -0400

    try with no &

--- kokoro/scripts/build/build_package.ps1
+++ kokoro/scripts/build/build_package.ps1
@@ -49,9 +49,9 @@ PACKAGE_VERSION,$env:PKG_VERSION
 Invoke-Program git submodule update --init
 $artifact_registry="us-docker.pkg.dev"
 Invoke-Program gcloud version
-& gcloud components update
+gcloud components update
 Invoke-Program gcloud version
-& gcloud auth configure-docker $artifact_registry
+gcloud auth configure-docker $artifact_registry
 
 $cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows"
 Invoke-Program docker build --cache-from="${cache_location}" -t $tag -f './Dockerfile.windows' .
