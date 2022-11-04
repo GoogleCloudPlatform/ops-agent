f39279c0dd59f3579a7d7c32a1a233f7bbbf2d7e
Martijn van Schaardenburg <martijnvs@google.com>
2022-11-04 12:54:37 -0400

    add --cache-from back

--- kokoro/scripts/build/build_package.ps1
+++ kokoro/scripts/build/build_package.ps1
@@ -54,7 +54,7 @@ Invoke-Program gcloud auth configure-docker $artifact_registry
 $cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows"
 Invoke-Program docker pull $cache_location
 
-Invoke-Program docker build -t $tag -f './Dockerfile.windows' .
+Invoke-Program docker build -t $tag -f './Dockerfile.windows' . --cache-from="$cache_location"
 Invoke-Program docker create --name $name $tag
 Invoke-Program docker cp "${name}:/work/out" $env:KOKORO_ARTIFACTS_DIR
 
