3587488e8bfb0f5f479bf64e2b97fa2fe7a829e3
Martijn van Schaardenburg <martijnvs@google.com>
2022-11-07 15:16:53 -0500

    Try skipping "update docker" step

--- kokoro/scripts/build/build_package.sh
+++ kokoro/scripts/build/build_package.sh
@@ -47,8 +47,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 ARTIFACT_REGISTRY="us-docker.pkg.dev"
 sudo gcloud auth configure-docker "${ARTIFACT_REGISTRY}"
