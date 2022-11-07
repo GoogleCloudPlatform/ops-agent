f7a2173907a9ddf7eff6bc6bf011e4830acbae5d
Jeff Erbrecht <jefferbrecht@google.com>
2022-11-07 10:00:23 -0500

    Fix printing parsed expectedMetrics
    The tests currently show something like this:
    
    ```
    2022/11/07 09:36:30 Parsed expectedMetrics: [0xc000114de0 0xc000115440 0xc000115500 0xc0001155c0 0xc000115680 0xc000115740 0xc000115800 0xc0001158c0 0xc000115980 0xc000115a40 0xc000115b00 0xc000115bc0 0xc000115c80 0xc000115d40 0xc000115e00 0xc000115ec0 0xc000115f80 0xc0000bab40]
    ```
    
    which is not super useful. This change formats pointers according to
    their underlying structure instead.

--- integration_test/third_party_apps_test.go
+++ integration_test/third_party_apps_test.go
@@ -462,7 +462,7 @@ func runLoggingTestCases(ctx context.Context, logger *logging.DirectoryLogger, v
 
 func runMetricsTestCases(ctx context.Context, logger *logging.DirectoryLogger, vm *gce.VM, metrics []*metadata.ExpectedMetric) error {
 	var err error
-	logger.ToMainLog().Printf("Parsed expectedMetrics: %+v", metrics)
+	logger.ToMainLog().Printf("Parsed expectedMetrics: %s", util.DumpPointerArray(metrics, "%+v"))
 	// Wait for the representative metric first, which is intended to *always*
 	// be sent. If it doesn't exist, we fail fast and skip running the other metrics;
 	// if it does exist, we go on to the other metrics in parallel, by which point they
@@ -673,7 +673,7 @@ func modifiedFiles(t *testing.T) []string {
 	stdout := string(out)
 	if err != nil {
 		stderr := ""
-        	if exitError := err.(*exec.ExitError); exitError != nil {
+		if exitError := err.(*exec.ExitError); exitError != nil {
 			stderr = string(exitError.Stderr)
 		}
 		t.Fatalf("got error calling `git diff`: %v\nstderr=%v\nstdout=%v", err, stderr, stdout)
@@ -753,7 +753,7 @@ const (
 	SAPHANAApp      = "saphana"
 
 	OracleDBPlatform = "rhel-7"
-	OracleDBApp = "oracledb"
+	OracleDBApp      = "oracledb"
 
 	AerospikeApp = "aerospike"
 )
@@ -773,10 +773,11 @@ func incompatibleOperatingSystem(testCase test) string {
 
 // When in `-short` test mode, mark some tests for skipping, based on
 // test_config and impacted apps.
-//   * For all impacted apps, test on all platforms.
-//   * Always test all apps against the default platform.
-//   * Always test the default app (postgres/active_directory_ds for now)
+//   - For all impacted apps, test on all platforms.
+//   - Always test all apps against the default platform.
+//   - Always test the default app (postgres/active_directory_ds for now)
 //     on all platforms.
+//
 // `platforms_to_skip` overrides the above.
 // Also, restrict `SAPHANAPlatform` to only test `SAPHANAApp` and skip that
 // app on all other platforms too. Same for `OracleDBPlatform` and
