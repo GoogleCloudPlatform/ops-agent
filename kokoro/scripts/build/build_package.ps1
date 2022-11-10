b60e96ce1abe27951c2c5c9217e448af301d3b3d
Martijn van Schaardenburg <martijnvs@google.com>
2022-11-10 16:13:36 -0500

    remove explicit pull of ltsc2019

--- kokoro/scripts/build/build_package.ps1
+++ kokoro/scripts/build/build_package.ps1
@@ -53,7 +53,7 @@ Invoke-Program gcloud auth configure-docker $artifact_registry
 
 $cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows-experiments2"
 Invoke-Program docker pull $cache_location
-Invoke-Program docker pull 'mcr.microsoft.com/windows/servercore:ltsc2019'
+# Invoke-Program docker pull 'mcr.microsoft.com/windows/servercore:ltsc2019'
 
 Invoke-Program docker build -t $tag -f './Dockerfile.windows' . --cache-from="$cache_location"
 Invoke-Program docker create --name $name $tag
