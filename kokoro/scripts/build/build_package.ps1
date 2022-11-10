bec9a05418cdf4491de9f460408153aba39f6a0c
Martijn van Schaardenburg <martijnvs@google.com>
2022-11-10 13:35:49 -0500

    strip down dockerfile, change tag

--- kokoro/scripts/build/build_package.ps1
+++ kokoro/scripts/build/build_package.ps1
@@ -52,22 +52,18 @@ $artifact_registry='us-docker.pkg.dev'
 Invoke-Program gcloud auth configure-docker $artifact_registry
 Invoke-Program docker images
 
-$cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows"
-Invoke-Program docker pull $cache_location
+$cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows-experiments"
+# Invoke-Program docker pull $cache_location
+
 Invoke-Program docker build --cache-from="${cache_location}" -t $tag -f './Dockerfile.windows' .
 Invoke-Program docker create --name $name $tag
 Invoke-Program docker cp "${name}:/work/out" $env:KOKORO_ARTIFACTS_DIR
 
-
-# Tell our continuous build to update the cache. Our other builds do not
-# write to any kind of cache, for example a per-PR cache, because the
-# push takes a few minutes and adds little value over just using the continuous
-# build's cache.
-if ($env:KOKORO_ROOT_JOB_TYPE -eq 'CONTINUOUS_INTEGRATION') {
-  Invoke-Program docker image tag $tag $cache_location
-  Invoke-Program docker push $cache_location
-}
+Invoke-Program docker image tag $tag $cache_location
+Invoke-Program docker push $cache_location
 
 # Copy the .goo file from $env:KOKORO_ARTIFACTS_DIR/out to $env:KOKORO_ARTIFACTS_DIR/result.
 New-Item -Path $env:KOKORO_ARTIFACTS_DIR -Name 'result' -ItemType 'directory'
 Move-Item -Path "$env:KOKORO_ARTIFACTS_DIR/out/*.goo" -Destination "$env:KOKORO_ARTIFACTS_DIR/result"
+
+Asdf
