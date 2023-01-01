37d4fb4dbbda3a665db1558f0e903115f64318a9
suffian khan <sufkha@google.com>
2023-01-01 16:55:30 +0000

    run go fmt

--- integration_test/third_party_apps_test.go
+++ integration_test/third_party_apps_test.go
@@ -790,7 +790,7 @@ const (
 	AerospikeApp = "aerospike"
 
 	NvmlApp = "nvml"
-   DcgmApp = "dcgm"
+	DcgmApp = "dcgm"
 )
 
 // incompatibleOperatingSystem looks at the supported_operating_systems field
@@ -855,7 +855,7 @@ func TestThirdPartyApps(t *testing.T) {
 	tests := []test{}
 	allApps := fetchAppsAndMetadata(t)
 	platforms := strings.Split(os.Getenv("PLATFORMS"), ",")
-	gpus := []string{"a100", "v100", /* "p100" todo: increase quota */ "t4", "p4"}
+	gpus := []string{"a100", "v100" /* "p100" todo: increase quota */, "t4", "p4"}
 
 	for _, platform := range platforms {
 		for app, metadata := range allApps {
