257fdf9c53fc14f73aba6d76662893428341df13
Francisco Valente <fcovalente@google.com>
2023-01-09 22:53:27 +0000

    Fix TestPassingHealthChecks.

--- integration_test/ops_agent_test.go
+++ integration_test/ops_agent_test.go
@@ -2684,17 +2684,17 @@ func TestPassingHealthChecks(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		serialPortLogName := "projects/stackdriver-test-143416/logs/serialconsole.googleapis.com%2Fserial_port_1_output"
-		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, , time.Hour, `textPayload="Check: Network Check, Result: PASS"`); err != nil {
+		serialPortLogName := "projects/stackdriver-test-143416/logs/serialconsole.googleapis.com%%2Fserial_port_1_output"
+		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, serialPortLogName, time.Hour, `textPayload="Check: Network Check, Result: PASS"`); err != nil {
 			t.Error(err)
 		}
-		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, , time.Hour, `textPayload="Check: API Check, Result: PASS"`); err != nil {
+		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, serialPortLogName, time.Hour, `textPayload="Check: API Check, Result: PASS"`); err != nil {
 			t.Error(err)
 		}
-		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, , time.Hour, `textPayload="Check: Permissions Check, Result: PASS"`); err != nil {
+		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, serialPortLogName, time.Hour, `textPayload="Check: Permissions Check, Result: PASS"`); err != nil {
 			t.Error(err)
 		}
-		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, , time.Hour, `textPayload="Check: Ports Check, Result: PASS"`); err != nil {
+		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, serialPortLogName, time.Hour, `textPayload="Check: Ports Check, Result: PASS"`); err != nil {
 			t.Error(err)
 		}
 	})
