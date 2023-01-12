fd093eb41635d5c0bcea661810f9c1f258cdc205
Francisco Valente <fcovalente@google.com>
2023-01-12 22:10:24 +0000

    Add error classification and ports error codes.

--- internal/health_checks/health_check_error.go
+++ internal/health_checks/health_check_error.go
@@ -16,6 +16,7 @@ package health_checks
 
 type HealthCheckError struct {
 	Code         string
+	Class        string
 	Message      string
 	Action       string
 	ResourceLink string
@@ -28,32 +29,59 @@ func (e HealthCheckError) Error() string {
 }
 
 var (
-	PORT_UNAVAILABLE_ERR = HealthCheckError{
-		Code:         "PORT_UNAVAILABLE_ERR",
-		Message:      "Port is unavailable",
-		Action:       "Check the port is available.",
-		ResourceLink: "",
+	FB_METRICS_PORT_ERR = HealthCheckError{
+		Code:         "FB_METRICS_PORT_ERR",
+		Class:        "PORT",
+		Message:      "Port 20202 used for fluent-bit self metrics is unavailable.",
+		Action:       "Verify the 20202 port is available.",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
+		IsFatal:      true,
+	}
+	OTEL_METRICS_PORT_ERR = HealthCheckError{
+		Code:         "OTEL_METRICS_PORT_ERR",
+		Class:        "PORT",
+		Message:      "Port 20201 used for open telemetry self metrics is unavailable.",
+		Action:       "Verify the 20201 port is available.",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
+		IsFatal:      true,
+	}
+	LOG_RECEIVER_PORT_ERR = HealthCheckError{
+		Code:         "LOG_RECEIVER_PORT_ERR",
+		Class:        "PORT",
+		Message:      "Port # used in logging receiver is unavailable.",
+		Action:       "Verify the # port is available.",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
 		IsFatal:      true,
 		// TODO : Add Message with specific port
-		// failMsg := fmt.Sprintf("listening to %s  was not successful.", net.JoinHostPort(host, port))
-		// solMsg := fmt.Sprintf("verify the host %s is available to be used.", net.JoinHostPort(host, port))
+	}
+	MON_RECEIVER_PORT_ERR = HealthCheckError{
+		Code:         "MON_RECEIVER_PORT_ERR",
+		Class:        "PORT",
+		Message:      "Port # used in metrics receiver is unavailable.",
+		Action:       "Verify the # port is available.",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
+		IsFatal:      true,
+		// TODO : Add Message with specific port
 	}
 	LOG_API_CONN_ERR = HealthCheckError{
 		Code:         "LOG_API_CONN_ERR",
-		Message:      "Request to Monitoring API failed.",
+		Class:        "CONNECTION",
+		Message:      "Request to Logging API failed.",
 		Action:       "Check your internet connection.",
-		ResourceLink: "",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
 		IsFatal:      true,
 	}
 	MON_API_CONN_ERR = HealthCheckError{
 		Code:         "MON_API_CONN_ERR",
+		Class:        "CONNECTION",
 		Message:      "Request to Monitoring API failed.",
 		Action:       "Check your internet connection.",
-		ResourceLink: "",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
 		IsFatal:      true,
 	}
 	LOG_API_PERMISSION_ERR = HealthCheckError{
 		Code:         "LOG_API_PERMISSION_ERR",
+		Class:        "PERMISSION",
 		Message:      "Service account misssing permissions for the Logging API.",
 		Action:       "Add the logging.writer role to the GCP service account.",
 		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting#logging-module-logs",
@@ -61,39 +89,44 @@ var (
 	}
 	MON_API_PERMISSION_ERR = HealthCheckError{
 		Code:         "MON_API_PERMISSION_ERR",
+		Class:        "PERMISSION",
 		Message:      "Service account misssing permissions for the Monitoring API.",
 		Action:       "Add the monitoring.writer role to the GCP service account.",
-		ResourceLink: "",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
 		IsFatal:      true,
 	}
 	LOG_API_DISABLED_ERR = HealthCheckError{
 		// TODO : Add Message with specific failure (e.g. Ping to api failed)
 		// c.Fail("logging client didn't Ping successfully.", "check the logging api is enabled.")
 		Code:         "LOG_API_DISABLED_ERR",
+		Class:        "API",
 		Message:      "The Logging API is disabled in the current GCP project.",
-		Action:       "Check the Logging API is enabled",
+		Action:       "Enable Logging API in the current GCP project.",
 		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting#logging-module-logs",
 		IsFatal:      true,
 	}
 	MON_API_DISABLED_ERR = HealthCheckError{
 		Code:         "MON_API_DISABLED_ERR",
-		Message:      "The Monitoring API disabled",
-		Action:       "Check the Monitoring API is disabled in the current GCP project.",
-		ResourceLink: "",
+		Class:        "API",
+		Message:      "The Monitoring API is disabled in the current GCP project.",
+		Action:       "Enable Monitoring API in the current GCP project.",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
 		IsFatal:      true,
 	}
 	CREDENTIALS_UNVERIFIABLE = HealthCheckError{
 		Code:         "CREDENTIALS_UNVERIFIABLE",
+		Class:        "PERMISSION",
 		Message:      "The provided credentials are unverifiable.",
 		Action:       "Check the result of the API Check.",
-		ResourceLink: "",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
 		IsFatal:      false,
 	}
 	HC_FAILURE_ERR = HealthCheckError{
 		Code:         "HC_FAILURE_ERR",
+		Class:        "GENERIC",
 		Message:      "The Health Check failed.",
-		Action:       "",
-		ResourceLink: "",
+		Action:       "There is ",
+		ResourceLink: "https://cloud.google.com/logging/docs/agent/ops-agent/troubleshooting",
 		IsFatal:      false,
 	}
 )
