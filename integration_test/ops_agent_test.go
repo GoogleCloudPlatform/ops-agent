803d4339d1b57a9fb42d3a6cef9311d8cf31815d
avilevy <avilevy@google.com>
2023-01-31 22:36:35 +0000

    Added integration test

--- integration_test/ops_agent_test.go
+++ integration_test/ops_agent_test.go
@@ -2672,6 +2672,18 @@ func TestLoggingAgentCrashRestart(t *testing.T) {
 	})
 }
 
+func TestLoggingFluentbitSelfLogs(t *testing.T) {
+	t.Parallel()
+	gce.RunForEachPlatform(t, func(t *testing.T, platform string) {
+		t.Parallel()
+		ctx, logger, vm := agents.CommonSetup(t, platform)
+
+		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, "ops-agent-fluent-bit", time.Hour, `severity="INFO"`); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
 func diagnosticsLivenessChecker(ctx context.Context, logger *log.Logger, vm *gce.VM) error {
 	time.Sleep(3 * time.Minute)
 	// Query for a metric sent by the diagnostics service from the last
