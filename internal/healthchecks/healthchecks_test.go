dd242996008e1dc59e5c4d91bdff0db328748cfe
Francisco Valente <fcovalente@google.com>
2023-02-01 21:26:52 +0000

    Refactor health checks logger and results.

--- internal/healthchecks/healthchecks_test.go
+++ internal/healthchecks/healthchecks_test.go
@@ -94,9 +94,9 @@ func TestRunAllHealthChecks(t *testing.T) {
 	eCheck := ErrorCheck{}
 	AllHealthChecks := healthchecks.HealthCheckRegistry{fCheck, sCheck, eCheck}
 
-	result := AllHealthChecks.RunAllHealthChecks("log")
+	result := AllHealthChecks.RunAllHealthChecks(testLogger)
 
-	assert.Check(t, strings.Contains(result[fCheck.Name()], "Result: FAIL"))
-	assert.Check(t, strings.Contains(result[sCheck.Name()], "Result: PASS"))
-	assert.Check(t, strings.Contains(result[eCheck.Name()], "Result: ERROR"))
+	assert.Check(t, strings.Contains(result[fCheck.Name()].Message, "Result: FAIL"))
+	assert.Check(t, strings.Contains(result[sCheck.Name()].Message, "Result: PASS"))
+	assert.Check(t, strings.Contains(result[eCheck.Name()].Message, "Result: ERROR"))
 }
