80523fc39723ee006caf3af3af4f021bc8fce145
avilevy <avilevy@google.com>
2023-02-02 15:34:03 +0000

    Attempting to test TimeOffset

--- integration_test/ops_agent_test.go
+++ integration_test/ops_agent_test.go
@@ -2682,6 +2682,14 @@ func TestLoggingFluentbitSelfLogs(t *testing.T) {
 			t.Fatal(err)
 		}
 
+		cmd := `sudo timedatectl set-timezone America/Toronto`
+		if gce.IsWindows(platform) {
+			cmd = `Set-TimeZone -Name "Eastern Standard Time"`
+		}
+		if _, err := gce.RunRemotely(ctx, logger.ToMainLog(), vm, "", cmd); err != nil {
+			t.Fatal(err)
+		}
+
 		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, "ops-agent-fluent-bit", time.Hour, `severity="INFO"`); err != nil {
 			t.Error(err)
 		}
