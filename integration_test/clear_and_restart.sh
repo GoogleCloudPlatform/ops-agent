af0957873c0cc7b06b1a171dbadad9b7889190f7
Francisco Valente <fcovalente@google.com>
2023-02-16 16:42:38 +0000

    Add another script.

--- integration_test/clear_and_restart.sh
+++ integration_test/clear_and_restart.sh
@@ -1,3 +1,5 @@
 sudo journalctl --rotate
 sudo journalctl --vacuum-time=1s
 sudo systemctl restart google-cloud-ops-agent.service
+
+sudo journalctl --rotate;sudo journalctl --vacuum-time=1s; sudo systemctl restart google-cloud-ops-agent.service
