7b83779dcef936ffcb131732d0ab73c19f548fb0
Jeff Erbrecht <89024676+jefferbrecht@users.noreply.github.com>
2023-03-07 15:43:40 -0500

    Trying another workaround to avoid flake (#1137)
    See b/255311117#comment36 for context.

--- integration_test/gce/gce_testing.go
+++ integration_test/gce/gce_testing.go
@@ -967,12 +967,15 @@ func addFrameworkMetadata(platform string, inputMetadata map[string]string) (map
 	metadataCopy["ssh-keys"] = fmt.Sprintf("%s:%s", sshUserName, string(publicKey))
 
 	if IsWindows(platform) {
-		// TODO(b/255311117): change windows-startup-script-cmd back to sysprep-specialize-script-cmd once the bug is fixed
-		if _, ok := metadataCopy["windows-startup-script-cmd"]; ok {
-			return nil, errors.New("you cannot pass a sysprep script for Windows instances because the sysprep script is needed to enable ssh-ing. Instead, wait for the instance to be ready and then run things with RunRemotely() or RunScriptRemotely()")
+		// TODO(b/255311117#comment38): move `googet update` back to sysprep-specialize-script-cmd once the bug is fixed
+		_, ok1 := metadataCopy["sysprep-specialize-script-cmd"]
+		_, ok2 := metadataCopy["windows-startup-script-cmd"]
+		if ok1 || ok2 {
+			return nil, errors.New("you cannot pass a sysprep or startup script for Windows instances because they are needed to enable ssh-ing. Instead, wait for the instance to be ready and then run things with RunRemotely() or RunScriptRemotely()")
 		}
 		// From https://cloud.google.com/compute/docs/connect/windows-ssh#create_vm
-		metadataCopy["windows-startup-script-cmd"] = "googet -noconfirm=true update && googet -noconfirm=true install google-compute-engine-ssh"
+		metadataCopy["sysprep-specialize-script-cmd"] = "googet -noconfirm=true install google-compute-engine-ssh"
+		metadataCopy["windows-startup-script-cmd"] = "googet -noconfirm=true update"
 
 		if _, ok := metadataCopy["enable-windows-ssh"]; ok {
 			return nil, errors.New("the 'enable-windows-ssh' metadata key is reserved for framework use")
