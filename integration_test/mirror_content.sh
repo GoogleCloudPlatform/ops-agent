a75a99bf01ca1f2d3ede2e95fc3b4db0e69c56e9
Martijn van Schaardenburg <martijnvs@google.com>
2023-04-27 12:46:57 -0600

    allow multiple URLs

--- integration_test/mirror_content.sh
+++ integration_test/mirror_content.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-USAGE="Usage: ./mirror_content.sh <URL>
+USAGE="Usage: ./mirror_content.sh [URL]...
 
-This will download the content at <URL> and upload it to:
+This will download the content at the given URLs and upload it to:
 
 https://storage.googleapis.com/ops-agents-public-buckets-vendored-deps/mirrored-content/<URL without http:// or https://>
 
 This script can only be run by Googlers because release tests read from the
 same bucket as presubmit tests, and we want release tests to be secure."
 
-if [[ "$#" -ne 1 ]]; then
+if [[ "$#" -eq 0 ]]; then
   echo "$USAGE"
   exit 1
 fi
 
 set -euxo pipefail
 
-URL="$1"
-
 SCRATCH="$(mktemp --directory)"
 
-(cd "${SCRATCH}" && wget --force-directories "${URL}")
+(cd "${SCRATCH}" && wget --force-directories "$@")
 
 gsutil cp -r "${SCRATCH}/*" "gs://ops-agents-public-buckets-vendored-deps/mirrored-content/"
 
-rm -r "${SCRATCH}"
-
 set +x
 
-STRIPPED_URL="$(echo "${URL}" | sed --regexp-extended 's#^https?://##')"
-echo "Content should be available at https://storage.googleapis.com/ops-agents-public-buckets-vendored-deps/mirrored-content/${STRIPPED_URL}"
+echo "Content should be available at"
+find "${SCRATCH}" -type f -printf 'https://storage.googleapis.com/ops-agents-public-buckets-vendored-deps/mirrored-content/%P\n'
+
+rm -r "${SCRATCH}"
