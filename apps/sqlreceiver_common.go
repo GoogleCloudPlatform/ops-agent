fe06523697c84b8e476e075f412c859c441e4d18
Sam DeHaan <sam.dehaan@bluemedora.com>
2023-05-02 13:33:27 -0400

    Add support for replication metrics in mysql (#1196)
    * Add support for replication metrics in mysql
    
    * Remove min versions from mysql 5.7 metadata
    
    * Update mysql 5.7 installs
    
    * remove mysql 8 config from mysql 57 integration exercise
    
    * Update mysql57 slow log expectations
    
    * Mark replica metrics as optional in kokoro until kokoro is testing a cluster
    
    * skip mysql 5.7 where not supported
    
    * Attempt to collect mysql replication metrics for 5.7
    
    * Touchups to mysql 5.7 exercise script
    
    * Keep iterating on kokoro mysql 5.7
    
    * Iterate on kokoro tests for mysql 5.7
    
    * Explicitly state datadir when initializing secondary mysql 5.7
    
    * Try again to fix mysql 5.7 in kokoro
    
    * Fix mysql socket name for 5.7 replica
    
    * ensure that mysql replica stays up after exercise script ends
    
    * switch nohup and sudo
    
    * attempt to resolve issues with mysql 5.7 source replica
    
    * Fix issues with centos7 mysql replication
    
    * Fix service name in centos mysql 5.7 build
    
    * debug changes
    
    * debug changes again
    
    * Give mysql replica time to start up
    
    * redirect all streams of nohup command so ssh command can terminate
    
    * Attempt to fix mysql metrics with allownativepasswords flag
    
    * fix error log expectation for mysql 5.7
    
    * Add replication metric integration test for mysql 8
    
    * Fix slow log parameter for mysql & add documentation of parameter
    
    * Fix find replace for master -> source for Mysql8
    
    * Update install script to work on rocky linux for mysql8
    
    * Fix focal mysql tests
    
    * Attempt to support sles integration tests for mysql
    
    * Fix path to install mysql 5.7 on sles
    
    * Disable sles15 for mysql5.7
    
    * Address PR feedback
    
    * remove the extra configuration for mysql replication support
    
    * remove special enable cfg from mysql5.7 kokoro
    
    * Reorganize mysql apps to emit pipelines in return
    
    * Rename sqlqueryreceiver file
    
    * Address PR feedback
    
    * Revert change, add back partial comment

---
+++ apps/sqlreceiver_common.go
@@ -0,0 +1,65 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package apps
+
+type sqlReceiverMetric struct {
+	metric_name       string
+	value_column      string
+	unit              string
+	description       string
+	data_type         string
+	monotonic         string
+	value_type        string
+	attribute_columns []string
+	static_attributes map[string]string
+}
+
+type sqlReceiverQuery struct {
+	query   string
+	metrics []sqlReceiverMetric
+}
+
+func sqlReceiverQueriesConfig(queries []sqlReceiverQuery) []map[string]interface{} {
+	cfg := []map[string]interface{}{}
+	for _, q := range queries {
+		metrics := []map[string]interface{}{}
+		for _, m := range q.metrics {
+			metric := map[string]interface{}{
+				"metric_name":       m.metric_name,
+				"value_column":      m.value_column,
+				"unit":              m.unit,
+				"description":       m.description,
+				"data_type":         m.data_type,
+				"value_type":        m.value_type,
+				"attribute_columns": m.attribute_columns,
+				"static_attributes": m.static_attributes,
+			}
+			if m.data_type == "sum" {
+				metric["monotonic"] = m.monotonic
+			}
+
+			metrics = append(metrics, metric)
+		}
+
+		query := map[string]interface{}{
+			"sql":     q.query,
+			"metrics": metrics,
+		}
+
+		cfg = append(cfg, query)
+	}
+
+	return cfg
+}
