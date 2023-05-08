49cd340b142622b697c73ce42398621819ede377
Jeff Erbrecht <89024676+jefferbrecht@users.noreply.github.com>
2023-05-08 13:29:55 -0400

    Support Dockerfile on ARM (#1208)
    * Add ARM support for the manual CMake download
    
    * Add ARM support for Golang download
    
    * Use build arg for CMake version
    
    * Minor rename for CMake layers
    
    * Use correct arch for OpenJDK
    
    * Disallow ARM on sles-15

--- dockerfiles/compile.go
+++ dockerfiles/compile.go
@@ -15,12 +15,19 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
 )
+
+//go:embed template
+var template string
+
+//go:embed template-header
+var templateHeader string
 
 type templateArguments struct {
 	from_image        string
@@ -46,6 +53,14 @@ func applyTemplate(template string, arguments templateArguments) string {
 	return template
 }
 
+// installCMake is used on platforms where the default package manager
+// does not provided a recent enough version of CMake (we require >= 3.12).
+// The cmake-install-recent layer is defined in template-header.
+const installCMake = `
+COPY --from=cmake-install-recent /cmake.sh /cmake.sh
+RUN set -x; bash /cmake.sh --skip-license --prefix=/usr/local
+`
+
 var dockerfileArguments = []templateArguments{
 	{
 		from_image:  "centos:7",
@@ -54,15 +69,8 @@ var dockerfileArguments = []templateArguments{
 		yum -y install git systemd \
 		autoconf libtool libcurl-devel libtool-ltdl-devel openssl-devel yajl-devel \
 		gcc gcc-c++ make bison flex file systemd-devel zlib-devel gtest-devel rpm-build java-11-openjdk-devel \
-		expect rpm-sign zip wget && \
-		yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
-		yum install -y cmake3 && \
-		ln -fs cmake3 /usr/bin/cmake && \
-		# Download and install CMake manually (zypper only gives us CMake 3.5, but we need >= 3.12)
-		wget https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-x86_64.sh -O cmake.sh && \
-		(echo '4d98de8d605da676e71a889dd94f80c76abb377fade2f21e3510e62ece1e1ada  cmake.sh' | sha256sum --check) && \
-		bash cmake.sh --skip-license --prefix=/usr/local
-		ENV JAVA_HOME /usr/lib/jvm/java-11-openjdk/`,
+		expect rpm-sign zip
+		ENV JAVA_HOME /usr/lib/jvm/java-11-openjdk/` + installCMake,
 		package_build:     "RUN ./pkg/rpm/build.sh",
 		tar_distro_name:   "centos-7",
 		package_extension: "rpm",
@@ -131,7 +139,7 @@ var dockerfileArguments = []templateArguments{
 		# The 'OSS Update' repo signature is no longer valid, so verify the checksum instead.
 		zypper --no-gpg-check refresh 'OSS Update' && \
 		(echo 'b889b4bba03074cd66ef9c0184768f4816d4ccb1fa9ec2721c5583304c5f23d0  /var/cache/zypp/raw/OSS Update/repodata/repomd.xml' | sha256sum --check) && \
-		zypper -n install git systemd autoconf automake flex libtool libcurl-devel libopenssl-devel libyajl-devel gcc gcc-c++ zlib-devel rpm-build expect cmake systemd-devel systemd-rpm-macros unzip zip wget && \
+		zypper -n install git systemd autoconf automake flex libtool libcurl-devel libopenssl-devel libyajl-devel gcc gcc-c++ zlib-devel rpm-build expect cmake systemd-devel systemd-rpm-macros unzip zip && \
 		# Remove expired root certificate.
 		mv /var/lib/ca-certificates/pem/DST_Root_CA_X3.pem /etc/pki/trust/blacklist/ && \
 		update-ca-certificates && \
@@ -143,18 +151,13 @@ var dockerfileArguments = []templateArguments{
 		# If this bug happens to trigger in the future, adding a "zypper -n download" of a subset of the packages can avoid the segfault.
 		zypper -n install bison>3.4 && \
 		# Allow fluent-bit to find systemd
-		ln -fs /usr/lib/systemd /lib/systemd && \
-		# Download and install CMake manually (zypper only gives us CMake 3.5, but we need >= 3.12)
-		wget https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-x86_64.sh -O cmake.sh && \
-		(echo '4d98de8d605da676e71a889dd94f80c76abb377fade2f21e3510e62ece1e1ada  cmake.sh' | sha256sum --check) && \
-		bash cmake.sh --skip-license --prefix=/usr/local
-	
-		ADD https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_x64_linux_hotspot_11.0.13_8.tar.gz /tmp/OpenJDK11U-jdk_x64_linux_hotspot_11.0.13_8.tar.gz
+		ln -fs /usr/lib/systemd /lib/systemd
+		COPY --from=openjdk-install /tmp/OpenJDK11U.tar.gz /tmp/OpenJDK11U.tar.gz
 		RUN set -xe; \
 			mkdir -p /usr/local/java-11-openjdk && \
-			tar -xf /tmp/OpenJDK11U-jdk_x64_linux_hotspot_11.0.13_8.tar.gz -C /usr/local/java-11-openjdk --strip-components=1
+			tar -xf /tmp/OpenJDK11U.tar.gz -C /usr/local/java-11-openjdk --strip-components=1
 		
-		ENV JAVA_HOME /usr/local/java-11-openjdk/`,
+		ENV JAVA_HOME /usr/local/java-11-openjdk/` + installCMake,
 		package_build:     "RUN ./pkg/rpm/build.sh",
 		tar_distro_name:   "sles-12",
 		package_extension: "rpm",
@@ -162,11 +165,12 @@ var dockerfileArguments = []templateArguments{
 	{
 		from_image:  "opensuse/leap:15.1",
 		target_name: "sles15",
-		install_packages: `RUN set -x; zypper -n install git systemd autoconf automake flex libtool libcurl-devel libopenssl-devel libyajl-devel gcc gcc-c++ zlib-devel rpm-build expect cmake systemd-devel systemd-rpm-macros java-11-openjdk-devel unzip zip wget
+		// TODO: Add ARM support to agent-vendor.repo.
+		install_packages: `RUN set -x; zypper -n install git systemd autoconf automake flex libtool libcurl-devel libopenssl-devel libyajl-devel gcc gcc-c++ zlib-devel rpm-build expect cmake systemd-devel systemd-rpm-macros java-11-openjdk-devel unzip zip
 		# Add agent-vendor.repo to install >3.4 bison
-		RUN echo $'[google-cloud-monitoring-sles15-x86_64-test] \n\
-		name=google-cloud-monitoring-sles15-x86_64-test \n\
-		baseurl=https://packages.cloud.google.com/yum/repos/google-cloud-monitoring-sles15-x86_64-test-20221109-1 \n\
+		RUN echo $'[google-cloud-monitoring-sles15-vendor] \n\
+		name=google-cloud-monitoring-sles15-vendor \n\
+		baseurl=https://packages.cloud.google.com/yum/repos/google-cloud-monitoring-sles15-$basearch-test-20221109-1 \n\
 		enabled         = 1 \n\
 		autorefresh     = 0 \n\
 		repo_gpgcheck   = 0 \n\
@@ -176,11 +180,7 @@ var dockerfileArguments = []templateArguments{
 			zypper -n update && \
 			zypper -n install bison>3.4 && \
 			# Allow fluent-bit to find systemd
-			ln -fs /usr/lib/systemd /lib/systemd && \
-			# Download and install CMake manually (zypper only gives us CMake 3.5, but we need >= 3.12)
-			wget https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-x86_64.sh -O cmake.sh && \
-			(echo '4d98de8d605da676e71a889dd94f80c76abb377fade2f21e3510e62ece1e1ada  cmake.sh' | sha256sum --check) && \
-			bash cmake.sh --skip-license --prefix=/usr/local`,
+			ln -fs /usr/lib/systemd /lib/systemd` + installCMake,
 		package_build:     "RUN ./pkg/rpm/build.sh",
 		tar_distro_name:   "sles-15",
 		package_extension: "rpm",
@@ -192,11 +192,7 @@ var dockerfileArguments = []templateArguments{
 		DEBIAN_FRONTEND=noninteractive apt-get -y install git systemd \
 		autoconf libtool libcurl4-openssl-dev libltdl-dev libssl-dev libyajl-dev \
 		build-essential bison flex file libsystemd-dev \
-		devscripts cdbs pkg-config openjdk-11-jdk zip wget && \
-		# Download and install CMake manually (zypper only gives us CMake 3.5, but we need >= 3.12)
-		wget https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-x86_64.sh -O cmake.sh && \
-		(echo '4d98de8d605da676e71a889dd94f80c76abb377fade2f21e3510e62ece1e1ada  cmake.sh' | sha256sum --check) && \
-		bash cmake.sh --skip-license --prefix=/usr/local`,
+		devscripts cdbs pkg-config openjdk-11-jdk zip` + installCMake,
 		package_build:     "RUN ./pkg/deb/build.sh",
 		tar_distro_name:   "ubuntu-bionic",
 		package_extension: "deb",
@@ -239,27 +235,6 @@ var dockerfileArguments = []templateArguments{
 	},
 }
 
-var dockerfileHeader = `# Copyright 2020 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file was generated by dockerfiles/compile.go
-# To re-generate, run from the repository root: go run ./dockerfiles
-
-# Build as DOCKER_BUILDKIT=1 docker build -o /tmp/out .
-# or DOCKER_BUILDKIT=1 docker build -o /tmp/out . --target=buster
-# Generated tarball(s) will end up in /tmp/out`
-
 func getDockerfileFooter() string {
 	components := []string{"FROM scratch"}
 	for _, arg := range dockerfileArguments {
@@ -273,18 +248,8 @@ func getDockerfilePath() string {
 	return filepath.Join(filepath.Dir(filepath.Dir(filename)), "Dockerfile")
 }
 
-func getTemplatePath() string {
-	_, filename, _, _ := runtime.Caller(0)
-	return filepath.Join(filepath.Dir(filename), "template")
-}
-
 func getDockerfile() (string, error) {
-	dat, err := os.ReadFile(getTemplatePath())
-	if err != nil {
-		return "", err
-	}
-	template := string(dat)
-	components := []string{dockerfileHeader}
+	components := []string{templateHeader}
 	for _, arg := range dockerfileArguments {
 		components = append(components, applyTemplate(template, arg))
 	}
