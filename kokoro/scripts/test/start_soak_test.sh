9a5c34ce704b476769df81896d306c1a14eebb84
Jeff Erbrecht <89024676+jefferbrecht@users.noreply.github.com>
2023-07-18 23:39:32 +0000

    Test experiment at 100 MB/s

--- kokoro/scripts/test/start_soak_test.sh
+++ kokoro/scripts/test/start_soak_test.sh
@@ -13,9 +13,10 @@ for GIT_ALIAS in git github; do
   fi
 done
 
-LOG_RATE=${LOG_RATE-1000} \
+LOG_RATE="100000" \
 LOG_SIZE_IN_BYTES=${LOG_SIZE_IN_BYTES-1000} \
 VM_NAME="${VM_NAME:-github-soak-test-${KOKORO_BUILD_NUMBER}}" \
 DISTRO="${DISTRO:-ubuntu-2004-lts}" \
-TTL="${TTL:-30m}" \
+TTL="360m" \
+AGENT_PACKAGES_IN_GCS="gs://ops-agents-public-buckets-test-logs/prod/stackdriver_agents/testing/consumer/ops_agent/build/focal/6146/20230718-161937/result" \
   go run -tags=integration_test .
