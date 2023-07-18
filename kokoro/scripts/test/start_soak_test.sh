97049df1bdc4a6ac653c72c10edbe8181a36262a
Jeff Erbrecht <89024676+jefferbrecht@users.noreply.github.com>
2023-07-18 23:22:23 +0000

    Test HEAD at 100 MB/s for baseline

--- kokoro/scripts/test/start_soak_test.sh
+++ kokoro/scripts/test/start_soak_test.sh
@@ -13,9 +13,10 @@ for GIT_ALIAS in git github; do
   fi
 done
 
-LOG_RATE=${LOG_RATE-1000} \
+LOG_RATE="100000" \
 LOG_SIZE_IN_BYTES=${LOG_SIZE_IN_BYTES-1000} \
 VM_NAME="${VM_NAME:-github-soak-test-${KOKORO_BUILD_NUMBER}}" \
 DISTRO="${DISTRO:-ubuntu-2004-lts}" \
-TTL="${TTL:-30m}" \
+TTL="360m" \
+REPO_SUFFIX="20230718-1" \
   go run -tags=integration_test .
