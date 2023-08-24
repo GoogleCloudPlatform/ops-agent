43ca24382f451172105c8dc67c4db0c135fe9f4d
Rafael Westphal <westphalrafael@google.com>
2023-08-24 18:13:56 +0000

    Multine line should be unique

--- confgenerator/logging_processors.go
+++ confgenerator/logging_processors.go
@@ -224,7 +224,7 @@ func (p LoggingProcessorParseMultilineRegex) Components(ctx context.Context, tag
 	filter := fluentbit.Component{
 		Kind: "FILTER",
 		Config: map[string]string{
-			"Name":                  "multiline",
+			"Name":                  fmt.Sprintf("multiline_%s", tag),
 			"Match":                 tag,
 			"Multiline.Key_Content": "message",
 			"Multiline.Parser":      multilineParserName,
