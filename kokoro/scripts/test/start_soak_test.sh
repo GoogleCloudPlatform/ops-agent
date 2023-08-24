eccae348b11cdde0b8d7e8b24b28bc050fab9df8
Martijn van Schaardenburg <martijnvs@google.com>
2023-08-24 16:12:11 -0400

    Testing: fix conflict for soak test presubmit

--- kokoro/scripts/test/start_soak_test.sh
+++ kokoro/scripts/test/start_soak_test.sh
@@ -15,6 +15,6 @@ done
 
 LOG_RATE=${LOG_RATE-1000} \
 LOG_SIZE_IN_BYTES=${LOG_SIZE_IN_BYTES-1000} \
-VM_NAME="${VM_NAME:-github-soak-test-${KOKORO_BUILD_NUMBER}}" \
+VM_NAME="${VM_NAME:-github-soak-test-${KOKORO_BUILD_ID}}" \
 TTL="${TTL:-30m}" \
   go run -tags=integration_test .
