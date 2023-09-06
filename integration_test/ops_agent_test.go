ce446f9242ff303a67ed35b936b0b503eac7a64a
Jeff Erbrecht <89024676+jefferbrecht@users.noreply.github.com>
2023-09-05 23:22:45 -0400

    Increase TCP input buffer from 32k to 512k (#1403)
    * Increase TCP input buffer from 32k to 512k
    
    * Add test helpers for background fluent-bit pipes
    
    * Refactor TCP test
    
    * Refactor forward test
    
    * Re-enable TCP test on Windows
    
    * Address review comment

--- integration_test/ops_agent_test.go
+++ integration_test/ops_agent_test.go
@@ -52,6 +52,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
@@ -1488,14 +1489,92 @@ func TestLogFilePathLabel(t *testing.T) {
 	})
 }
 
+// startFluentBitBackgroundPipe starts a background fluent-bit process that tails a file (whose
+// path is returned by this function) and pipes its contents to an output configured via outputConfig.
+// An example outputConfig would be "-o tcp://127.0.0.1:5170".
+// Use parseInputAsJSON for structured input.
+func startFluentBitBackgroundPipe(ctx context.Context, logger *logging.DirectoryLogger, vm *gce.VM, platform string, parseInputAsJSON bool, outputConfig string) (string, error) {
+	dir := workDirForPlatform(platform)
+	if err := makeDirectory(ctx, logger, vm, dir); err != nil {
+		return "", err
+	}
+	remoteFile := filepath.Join(dir, "pipe.log")
+	parserFile := filepath.Join(dir, "parser.conf")
+	parserConfig := `[PARSER]
+	Name json
+	Format json`
+
+	fluentBitArgs := "-i tail" +
+		" -p buffer_chunk_size=512k" +
+		" -p buffer_max_size=512k" +
+		" -p path=" + remoteFile
+	if parseInputAsJSON {
+		fluentBitArgs += " -p parser=json" +
+			" -R " + parserFile
+	}
+	fluentBitArgs += " " + outputConfig
+
+	// Create the pipe file, create the parser file, and start fluent-bit in the background.
+	// The parser needs to be stored to a file because fluent-bit doesn't support configuring
+	// parsers on the command line.
+	command := fmt.Sprintf(`
+		sudo touch %s
+		sudo tee %s > /dev/null <<EOF
+%s
+EOF
+		sudo nohup /opt/google-cloud-ops-agent/subagents/fluent-bit/bin/fluent-bit %s 1>/dev/null 2>/dev/null &`,
+		remoteFile,
+		parserFile,
+		parserConfig,
+		// Escape record accessor dollar-signs
+		strings.ReplaceAll(fluentBitArgs, "$", `\$`))
+	if gce.IsWindows(platform) {
+		command = fmt.Sprintf(`
+			New-Item %s
+			"%s" | Out-File -Encoding Ascii %s
+			Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList 'C:\Program Files\Google\Cloud Operations\Ops Agent\bin\fluent-bit.exe %s'`,
+			remoteFile,
+			parserConfig,
+			parserFile,
+			fluentBitArgs)
+	}
+
+	if _, err := gce.RunRemotely(ctx, logger.ToMainLog(), vm, "", command); err != nil {
+		return "", err
+	}
+	return remoteFile, nil
+}
+
+// writeLinesToRemoteFile writes lines of text to a remote file.
+// Lines each have an implicit terminating newline character.
+// Lines are allowed to be huge.
+func writeLinesToRemoteFile(ctx context.Context, logger *logging.DirectoryLogger, vm *gce.VM, platform string, remoteFile string, lines ...string) error {
+	for _, line := range lines {
+		line += "\n"
+
+		// Use a temp-file as a buffer to allow for long lines.
+		tempPath := filepath.Join(workDirForPlatform(platform), "pipe_temp.log")
+		if err := gce.UploadContent(ctx, logger.ToMainLog(), vm, strings.NewReader(line), tempPath); err != nil {
+			return err
+		}
+
+		// Append the temp-file to the remote file.
+		appendCommand := fmt.Sprintf(`sudo cat %s | sudo tee -a %s > /dev/null`, tempPath, remoteFile)
+		if gce.IsWindows(platform) {
+			appendCommand = fmt.Sprintf(`Get-Content %s | Out-File -Encoding Ascii -Append %s`, tempPath, remoteFile)
+		}
+
+		if _, err := gce.RunRemotely(ctx, logger.ToMainLog(), vm, "", appendCommand); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func TestTCPLog(t *testing.T) {
 	t.Parallel()
 	gce.RunForEachPlatform(t, func(t *testing.T, platform string) {
 		t.Parallel()
-		if gce.IsWindows(platform) {
-			// TODO: Delete when b/285865631 is fixed.
-			t.SkipNow()
-		}
 
 		ctx, logger, vm := agents.CommonSetup(t, platform)
 
@@ -1515,50 +1594,45 @@ func TestTCPLog(t *testing.T) {
 		if err := agents.SetupOpsAgent(ctx, logger.ToMainLog(), vm, config); err != nil {
 			t.Fatal(err)
 		}
-		command := `echo '{"msg":"test tcp log 1"}{"msg":"test tcp log 2"}' | /opt/google-cloud-ops-agent/subagents/fluent-bit/bin/fluent-bit -i stdin -o tcp://127.0.0.1:5170 -p format=json_lines && echo '{"msg":"test tcp log 3"}{"msg":"test tcp log 4"}' | /opt/google-cloud-ops-agent/subagents/fluent-bit/bin/fluent-bit -i stdin -o tcp://127.0.0.1:5170 -p format=json_lines`
-		// Installing Netcat or equivalent tools on Windows is tricky so we can instead
-		// use this Powershell Script that can send TCP messages.
-		if gce.IsWindows(platform) {
-			command = `$ErrorActionPreference = 'Stop'
-			function Send-TcpString {
-			  param(
-			    [string]$TargetIP,
-			    [int]$TargetPort,
-			    [string]$Message
-			  )
-			  $Socket = New-Object System.Net.Sockets.TCPClient($TargetIP, $TargetPort) 
-			  $Stream = $Socket.GetStream() 
-			  $Writer = New-Object System.IO.StreamWriter($Stream)
-			  $Message | % {
-			    $Writer.WriteLine($_)
-			    $Writer.Flush()
-			  }
-			  $Stream.Close()
-			  $Socket.Close()
-			}
-			Send-TcpString 127.0.0.1 5170 '{"msg":"test tcp log 1"}{"msg":"test tcp log 2"}'
-			Send-TcpString 127.0.0.1 5170 '{"msg":"test tcp log 3"}{"msg":"test tcp log 4"}'`
+
+		// Start a background fluent-bit that outputs to TCP.
+		// The TCP receiver in the Ops Agent already parses to JSON,
+		// so don't double-parse it in the background fluent-bit.
+		pipePath, err := startFluentBitBackgroundPipe(ctx, logger, vm, platform, false, "-o tcp://127.0.0.1:5170 -p raw_message_key=$log")
+		if err != nil {
+			t.Fatalf("Error starting fluent-bit background pipe: %v", err)
 		}
 
-		// Write JSON test log to TCP socket via bash redirect, to get around installing and using netcat.
-		// https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Redirections
+		linesToWrite := []string{
+			// Verify that separate JSON messages are partitioned appropriately,
+			// regardless of where the newlines appear between messages.
+			`{"msg":"test tcp log 1"}{"msg":"test tcp log 2"}`,
+			`{"msg":"test tcp log 3"}{"msg":"test tcp log 4"}`,
 
-		if _, err := gce.RunRemotely(ctx, logger.ToMainLog(), vm, "", command); err != nil {
-			t.Fatalf("Error writing dummy TCP log line: %v", err)
+			// Verify a large log that's reasonably close to the limit of 256 KB.
+			// Use "start" and "end" for querying later because the max query size
+			// is only 20 KB.
+			fmt.Sprintf(`{"large":"start%send"}`, strings.Repeat("a", 250_000)),
+		}
+		if err = writeLinesToRemoteFile(ctx, logger, vm, platform, pipePath, linesToWrite...); err != nil {
+			t.Fatalf("Error writing dummy TCP log lines: %v", err)
 		}
 
 		var waitGroup sync.WaitGroup
-		for i := 1; i <= 4; i++ {
-			i := i
+		addQueryToWaitGroup := func(query string) {
 			waitGroup.Add(1)
 			go func() {
 				defer waitGroup.Done()
-				if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, "tcp_logs", time.Hour, fmt.Sprintf("jsonPayload.msg:test tcp log %d", i)); err != nil {
+				if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, "tcp_logs", time.Hour, query); err != nil {
 					t.Error(err)
 				}
 			}()
-
 		}
+		addQueryToWaitGroup(`jsonPayload.msg="test tcp log 1"`)
+		addQueryToWaitGroup(`jsonPayload.msg="test tcp log 2"`)
+		addQueryToWaitGroup(`jsonPayload.msg="test tcp log 3"`)
+		addQueryToWaitGroup(`jsonPayload.msg="test tcp log 4"`)
+		addQueryToWaitGroup(`jsonPayload.large:"start" AND jsonPayload.large:"end"`)
 		waitGroup.Wait()
 	})
 }
@@ -1585,23 +1659,21 @@ func TestFluentForwardLog(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		// Use another instance of Fluent Bit to read from stdin and  forward to the
-		// Ops Agent.
-		//
-		// The forwarding Fluent Bit uses the tag "forwarder_tag" when sending the
-		// log record. This will be preserved in the LogName.
-
-		command := "echo '{\"rand_value\":\"test fluent forward log\"}' | /opt/google-cloud-ops-agent/subagents/fluent-bit/bin/fluent-bit -i stdin -o forward://127.0.0.1:24224 -t forwarder_tag"
-
-		if gce.IsWindows(platform) {
-			command = `Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList "C:\Program Files\Google\Cloud Operations\Ops Agent\bin\fluent-bit.exe -i random -o forward://127.0.0.1:24224 -t forwarder_tag"`
+		// Start a background fluent-forward pipe. We want to verify that
+		// the log structure is preserved, so enable JSON parsing.
+		var pipePath string
+		var err error
+		if pipePath, err = startFluentBitBackgroundPipe(ctx, logger, vm, platform, true, "-o forward://127.0.0.1:24224 -t forwarder_tag"); err != nil {
+			t.Fatalf("Error starting fluent-bit background pipe: %v", err)
 		}
 
-		if _, err := gce.RunRemotely(ctx, logger.ToMainLog(), vm, "", command); err != nil {
-			t.Fatalf("Error writing dummy forward protocol log line %v", err)
+		// Verify a large structured log that's reasonably close to the limit of 256 KB.
+		largeLog := fmt.Sprintf(`{"large":"start%send"}`, strings.Repeat("a", 250_000))
+		if err = writeLinesToRemoteFile(ctx, logger, vm, platform, pipePath, largeLog); err != nil {
+			t.Fatalf("Error writing dummy TCP log line: %v", err)
 		}
 
-		if err := gce.WaitForLog(ctx, logger.ToMainLog(), vm, "fluent_logs.forwarder_tag", time.Hour, "jsonPayload.rand_value:*"); err != nil {
+		if err = gce.WaitForLog(ctx, logger.ToMainLog(), vm, "fluent_logs.forwarder_tag", time.Hour, `jsonPayload.large:"start" AND jsonPayload.large:"end"`); err != nil {
 			t.Error(err)
 		}
 	})
