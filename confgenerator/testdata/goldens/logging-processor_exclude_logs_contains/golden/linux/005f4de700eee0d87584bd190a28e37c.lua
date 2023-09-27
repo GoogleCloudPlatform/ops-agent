f25bf7accba5dba255895916929ec778e44984aa
Jeff Erbrecht <jefferbrecht@google.com>
2023-09-27 17:51:13 -0400

    Always update log record after exclusion
    The inner logic for an exclusion filter relies on the creation of
    temporary `__match_` fields. Those fields must be removed before the
    filter terminates. The existing lua code was returning `0, 0, 0` in the
    event that a log doesn't match the exclusion filter, which means "don't
    modify the record." But because we were also removing the `__match_`
    fields inside the same lua function, those removals are not being
    committed back to the log record afterwards.
    
    Simple fix is to return `2, 0, record` instead.

---
+++ confgenerator/testdata/goldens/logging-processor_exclude_logs_contains/golden/linux/005f4de700eee0d87584bd190a28e37c.lua
@@ -0,0 +1,11 @@
+
+function process(tag, timestamp, record)
+local match = ((function(v) if v == nil then return false end return (string.find(string.lower(tostring(v)), string.lower("IN"), 1, false) ~= nil) end)((function()
+return record["Status"]
+end)()));
+
+  if match then
+    return -1, 0, 0
+  end
+  return 2, 0, record
+end
