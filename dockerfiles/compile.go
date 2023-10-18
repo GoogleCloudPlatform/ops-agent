ff0d020829b2f9fff9833835ef1fb9f074fed980
Sophie Fang <sophieyfang@google.com>
2023-10-18 15:52:23 -0400

    Support Ubuntu 23.10 mantic

--- dockerfiles/compile.go
+++ dockerfiles/compile.go
@@ -223,6 +223,18 @@ RUN ln -fs /usr/lib/systemd /lib/systemd` + installJava + installCMake,
 		tar_distro_name:   "ubuntu-lunar",
 		package_extension: "deb",
 	},
+	{
+		from_image:  "ubuntu:mantic",
+		target_name: "mantic",
+		install_packages: `RUN set -x; apt-get update && \
+		DEBIAN_FRONTEND=noninteractive apt-get -y install git systemd \
+		autoconf libtool libcurl4-openssl-dev libltdl-dev libssl-dev libyajl-dev \
+		build-essential cmake bison flex file libsystemd-dev \
+		devscripts cdbs pkg-config openjdk-${OPENJDK_MAJOR_VERSION}-jdk zip debhelper`,
+		package_build:     "RUN ./pkg/deb/build.sh",
+		tar_distro_name:   "ubuntu-mantic",
+		package_extension: "deb",
+	},
 }
 
 func getDockerfileFooter() string {
