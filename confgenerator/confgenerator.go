a2f37109cea8f5245cf28c29bf185c680be44a54
Lujie Duan <lujieduan@google.com>
2023-10-30 23:53:36 +0000

    Remove deprecated retry_on_failure on exporter

--- confgenerator/confgenerator.go
+++ confgenerator/confgenerator.go
@@ -35,12 +35,6 @@ func googleCloudExporter(userAgent string, instrumentationLabels bool) otel.Comp
 	return otel.Component{
 		Type: "googlecloud",
 		Config: map[string]interface{}{
-			// (b/233372619) Due to a constraint in the Monarch API for retrying successful data points,
-			// leaving this enabled is causing adverse effects for some customers. Google OpenTelemetry team
-			// recommends disabling this.
-			"retry_on_failure": map[string]interface{}{
-				"enabled": false,
-			},
 			"user_agent": userAgent,
 			"metric": map[string]interface{}{
 				// Receivers are responsible for sending fully-qualified metric names.
