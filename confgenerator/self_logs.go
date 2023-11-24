ae8dcabec2e1c5107b41b75bef02744ca3bcc3f0
Francisco Valente <1435136+franciscovalentecastro@users.noreply.github.com>
2023-11-24 18:57:46 +0000

    Set initial timestamp.

--- confgenerator/self_logs.go
+++ confgenerator/self_logs.go
@@ -18,7 +18,9 @@ import (
 	"context"
 	"fmt"
 	"path"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/fluentbit"
 	"github.com/GoogleCloudPlatform/ops-agent/internal/logs"
@@ -65,6 +67,8 @@ func generateHealthLoggingPingComponent(ctx context.Context) []fluentbit.Compone
 				"Dummy":         `{"code": "LogPingOpsAgent", "severity": "DEBUG"}`,
 				"Interval_Sec":  "600",
 				"Interval_NSec": "0",
+				"Start_time_sec": strconv.FormatInt(time.Now().Unix(), 10),
+				"Start_time_nsec": "0",
 			},
 		},
 	}
