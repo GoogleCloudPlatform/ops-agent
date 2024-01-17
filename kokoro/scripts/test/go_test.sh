21a453b2c21c35f4df486c945fa1b74f4d7bb40f
Corey Sarsfield <subbarker@gmail.com>
2024-01-17 17:17:05 -0500

    slightly better comment

--- kokoro/scripts/test/go_test.sh
+++ kokoro/scripts/test/go_test.sh
@@ -65,7 +65,7 @@ function set_platforms() {
     return 0
   fi
   # if _LOUHI_TAG_NAME is defined, set TARGET and ARCH env vars by parsing it.
-  # Example value: louhi/2.46.0/foobar/windows/x86_64/start
+  # Example value: louhi/2.46.0/shortref/windows/x86_64/start
   if [ -n "${_LOUHI_TAG_NAME}" ]; then
     local split_tag=(${_LOUHI_TAG_NAME//\// })
     TARGET="${split_tag[3]}"
