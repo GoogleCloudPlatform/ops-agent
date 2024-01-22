ccbfdb1b311138e8b8d7a6abb002afb6fcbcf8ae
Corey Sarsfield <subbarker@gmail.com>
2024-01-22 13:51:02 -0500

    reverting everything but the PR caching logic

--- kokoro/scripts/build/build_package.sh
+++ kokoro/scripts/build/build_package.sh
@@ -50,23 +50,40 @@ if [[ -n "${KOKORO_GITHUB_PULL_REQUEST_NUMBER}" ]]; then  # Per-PR cache
     build_params+=(--cache-from=type=registry,ref="${CACHE}:${DISTRO}_${ARCH}_PR${KOKORO_GITHUB_PULL_REQUEST_NUMBER}")
     build_params+=(--cache-to=type=registry,ref="${CACHE}:${DISTRO}_${ARCH}_PR${KOKORO_GITHUB_PULL_REQUEST_NUMBER}",mode=max)
 fi
-if [[ "${KOKORO_ROOT_JOB_TYPE}" == "CONTINUOUS_INTEGRATION" ]]; then  # CI cache
-    build_params+=(--cache-to=type=registry,ref="${CACHE}:${DISTRO}_${ARCH}",mode=max)
-fi
 
 docker buildx create --use
 docker buildx build . \
-  --cache-from=type=registry,ref="${CACHE}:${DISTRO}_${ARCH}" \
+  --cache-from="${CACHE}:${DISTRO}_${ARCH}" \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   --progress=plain \
-  --target "${DISTRO}" \
-  -o "${RESULT_DIR}" \
+  --target "${DISTRO}-build" \
+  -t build_image \
   "${build_params[@]}"
+
+# Tell our continuous build to update the cache. Our other builds do not
+# write to any kind of cache, for example a per-PR cache, because the
+# push takes a few minutes and adds little value over just using the continuous
+# build's cache.
+if [[ "${KOKORO_ROOT_JOB_TYPE}" == "CONTINUOUS_INTEGRATION" ]]; then
+  docker image tag build_image "${CACHE_LOCATION}"
+  docker push "${CACHE_LOCATION}"
+fi
+
+SIGNING_DIR="$(pwd)/kokoro/scripts/build/signing"
 
 if [[ "${PKGFORMAT}" == "rpm" && "${SKIP_SIGNING}" != "true" ]]; then
   RPM_SIGNING_KEY="${KOKORO_KEYSTORE_DIR}/71565_rpm-signing-key"
-  SIGNING_DIR="$(pwd)/kokoro/scripts/build/signing"
-
   cp "${RPM_SIGNING_KEY}" "${SIGNING_DIR}/signing-key"
-  bash "${SIGNING_DIR}"/sign.sh "${RESULT_DIR}"/*.rpm
 fi
+
+docker run \
+  -i \
+  -v "${RESULT_DIR}":/artifacts \
+  -v "${SIGNING_DIR}":/signing \
+  build_image \
+  bash <<EOF
+    cp /google-cloud-ops-agent*.${PKGFORMAT} /artifacts
+    if [[ "${PKGFORMAT}" == "rpm" && "${SKIP_SIGNING}" != "true" ]]; then
+      bash /signing/sign.sh /artifacts/*.rpm
+    fi
+EOF
