914040cf8fc93f356aa6bd3699fe7c2303258d53
Corey Sarsfield <subbarker@gmail.com>
2024-01-23 17:27:08 -0500

    initial commit

--- kokoro/scripts/test/go_test.sh
+++ kokoro/scripts/test/go_test.sh
@@ -66,22 +66,22 @@ else:
 
 function set_platforms() {
   # if PLATFORMS is defined, do nothing
-  if [[ -n "${PLATFORMS}" ]]; then
+  if [[ -n "${PLATFORMS:-}" ]]; then
     return 0
   fi
   # if _LOUHI_TAG_NAME is defined, set TARGET and ARCH env vars by parsing it.
   # Example value: louhi/2.46.0/shortref/windows/x86_64/start
-  if [[ -n "${_LOUHI_TAG_NAME}" ]]; then
+  if [[ -n "${_LOUHI_TAG_NAME:-}" ]]; then
     TARGET="$(echo -n "${_LOUHI_TAG_NAME}" | cut --delimiter="/" --fields=4)"
     ARCH="$(echo -n "${_LOUHI_TAG_NAME}" | cut --delimiter="/" --fields=5)"
   fi
   # if TARGET is not set, return an error
-  if [[ -z "${TARGET}" ]]; then
+  if [[ -z "${TARGET:-}" ]]; then
     echo "At least one of TARGET/PLATFORMS must be set." 1>&2
     return 1
   fi
   # if ARCH is not set, return an error
-  if [[ -z "${ARCH}" ]]; then
+  if [[ -z "${ARCH:-}" ]]; then
     echo "If TARGET is set, ARCH must be as well." 1>&2
     return 1
   fi
