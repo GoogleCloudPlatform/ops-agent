292741ce52eed06c98257b3f140d58b573c93abe
avilevy18 <105948922+avilevy18@users.noreply.github.com>
2024-02-08 13:03:12 -0500

    Updated error message
    Co-authored-by: Jeff Erbrecht <89024676+jefferbrecht@users.noreply.github.com>

--- confgenerator/prometheus.go
+++ confgenerator/prometheus.go
@@ -229,7 +229,7 @@ func validatePrometheus(promConfig promconfig.Config) (string, error) {
 
 	for _, sc := range promConfig.ScrapeConfigs {
 		if sc.ScrapeInterval < MinScrapeInterval {
-			return "scrape_interval", fmt.Errorf("error validation scrap_config for job %v: scrape_interval must be equal to or greater than %v", sc.JobName, MinScrapeInterval.String())
+			return "scrape_interval", fmt.Errorf("error validating scrape_config for job %v: scrape_interval must be equal to or greater than %v", sc.JobName, MinScrapeInterval.String())
 		}
 		if sc.HonorLabels {
 			return "honor_labels", fmt.Errorf("error validating scrape_config for job %v: %v", sc.JobName, "honor_labels is not supported")
