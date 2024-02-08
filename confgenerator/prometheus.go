940ed098380d3d14356c4e313ece7279dbe3e124
avilevy <avilevy@google.com>
2024-02-08 19:43:58 +0000

    Addressed comments, validation will no longer stop config generation, will override users config to meet minimum accepted value instead.

--- confgenerator/prometheus.go
+++ confgenerator/prometheus.go
@@ -37,7 +37,7 @@ import (
 	_ "github.com/prometheus/prometheus/discovery/install" // init() of this package registers service discovery impl.
 )
 
-const MinScrapeInterval model.Duration = model.Duration(5 * time.Second)
+const minScrapeInterval = model.Duration(5 * time.Second)
 
 type PrometheusMetrics struct {
 	ConfigComponent `yaml:",inline"`
@@ -228,9 +228,10 @@ func validatePrometheus(promConfig promconfig.Config) (string, error) {
 	}
 
 	for _, sc := range promConfig.ScrapeConfigs {
-		if sc.ScrapeInterval < MinScrapeInterval {
-			sc.ScrapeInterval = MinScrapeInterval
-			log.Println("scrape_interval must be at least 5s; adjusting to minimum accepted value")
+		if sc.ScrapeInterval < minScrapeInterval {
+			sc.ScrapeInterval = minScrapeInterval
+			sc.ScrapeTimeout = minScrapeInterval
+			log.Printf("scrape_interval must be at least %v; adjusting to minimum accepted value\n", minScrapeInterval)
 		}
 		if sc.HonorLabels {
 			return "honor_labels", fmt.Errorf("error validating scrape_config for job %v: %v", sc.JobName, "honor_labels is not supported")
