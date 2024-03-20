41e8f795286dcf061b2ba121926628c6614dbc5c
Corey Sarsfield <subbarker@gmail.com>
2024-03-20 12:54:48 -0400

    mover comment

--- kokoro/scripts/test/go_test.sh
+++ kokoro/scripts/test/go_test.sh
@@ -207,9 +207,10 @@ if [[ "${SHORT:-false}" == "true" ]]; then
   args+=( "-test.short" )
 fi
 
+ # GOTESTSUM_ARGS is intentionally unquoted
 TEST_UNDECLARED_OUTPUTS_DIR="${LOGS_DIR}" \
   gotestsum \
-  ${GOTESTSUM_ARGS:-} \ # intentionally unquoted
+  ${GOTESTSUM_ARGS:-} \
   --packages=./"${TEST_SUITE_NAME}.go" \
   --format=standard-verbose \
   --junitfile="${LOGS_DIR}/sponge_log.xml" \
