3b5ea8ba759340f030a7749e294fd5a5bc38fb1a
Corey Sarsfield <subbarker@gmail.com>
2024-04-15 15:05:14 -0400

    specify test.go

--- kokoro/scripts/test/go_test.sh
+++ kokoro/scripts/test/go_test.sh
@@ -208,7 +208,7 @@ ulimit -n 1000000
 
 # Set up some command line flags for "gotestsum".
 gotestsum_args=(
-  --packages=./"${TEST_SUITE_NAME}/..."
+  --packages=./"${TEST_SUITE_NAME}/test.go"
   --format=standard-verbose
   --junitfile="${LOGS_DIR}/sponge_log.xml"
 )
