0572082dbed8f1adcb3528416c92e54f4cd6e180
braydonk <braydonk@google.com>
2024-05-24 13:21:33 +0000

    move shared lib comment

--- builds/fluent_bit.sh
+++ builds/fluent_bit.sh
@@ -18,6 +18,7 @@ set -x -e
 DESTDIR="$1"
 mkdir -p $DESTDIR
 
+# -DFLB_SHARED_LIB=OFF skips building libfluent-bit.so.
 mapfile -d '' CMAKE_ARGS <<-END
 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DFLB_HTTP_SERVER=ON -DFLB_DEBUG=OFF \
 -DWITHOUT_HEADERS=ON -DFLB_SHARED_LIB=OFF -DFLB_STREAM_PROCESSOR=OFF \
@@ -40,7 +41,6 @@ else
   fluent_bit_dir=/opt/google-cloud-ops-agent/subagents/fluent-bit
   # CMAKE_INSTALL_PREFIX here will cause the binary to be put at
   # /usr/lib/google-cloud-ops-agent/bin/fluent-bit
-  # Additionally, -DFLB_SHARED_LIB=OFF skips building libfluent-bit.so
   cmake .. -DCMAKE_INSTALL_PREFIX=$fluent_bit_dir $CMAKE_ARGS
   make -j8
   make DESTDIR="$DESTDIR" install
