532e4f85bd7b48d036139148230eb1608f7335cb
Corey Sarsfield <subbarker@gmail.com>
2024-05-31 16:35:23 -0400

    inital commit

--- kokoro/scripts/test/start_soak_test.sh
+++ kokoro/scripts/test/start_soak_test.sh
@@ -5,6 +5,13 @@ set -u
 set -x
 set -o pipefail
 
+# cd to the root of the git repo containing this script ($0).
+cd "$(readlink -f "$(dirname "$0")")"
+cd ../../../
+
+# Source the common utilities.
+source kokoro/scripts/utils/louhi.sh
+
 for GIT_ALIAS in git github; do
   CANDIDATE_LOCATION="${KOKORO_ARTIFACTS_DIR}/${GIT_ALIAS}/unified_agents/integration_test/soak_test/cmd/launcher"
   if [[ -d "${CANDIDATE_LOCATION}" ]]; then
@@ -12,6 +19,16 @@ for GIT_ALIAS in git github; do
     break
   fi
 done
+
+# For soak tests run by Louhi
+read_louhi_tag
+# if TARGET & ARCH are set, retrieve the soak distro from project.yaml
+if [[ -n "${TARGET:-}" && -n "${ARCH:-}" ]]; then
+  DISTRO=$(yaml project.yaml "['targets']['${TARGET}']['architectures']['${ARCH}']['soak_distro']")
+  VM_NAME="soak-test-${REPO_SUFFIX}-${LABEL}"
+  export DISTRO
+  export VM_NAME
+fi
 
 LOG_RATE=${LOG_RATE-1000} \
 LOG_SIZE_IN_BYTES=${LOG_SIZE_IN_BYTES-1000} \
---
+++ kokoro/scripts/utils/louhi.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A helper for parsing YAML files.
+# Ex: VALUE=$(yaml ~/my_yaml_file.yaml "['a_key']")
+function yaml() {
+  python3 -c "import yaml
+data=yaml.safe_load(open('$1'))$2
+if type(data)==list:
+  print(','.join(str(elem) for elem in data))
+else:
+ print(data)"
+}
+
+function read_louhi_tag() {
+  # if _LOUHI_TAG_NAME is defined, set TARGET and ARCH env vars by parsing it.
+  # Example value: louhi/2.46.0/shortref/windows/x86_64/start
+  if [[ -n "${_LOUHI_TAG_NAME:-}" ]]; then
+    local -a _LOUHI_TAG_COMPONENTS=(${_LOUHI_TAG_NAME//\// })  
+    export REPO_SUFFIX="${_LOUHI_TAG_COMPONENTS[2]}"  # the shortref is the repo suffix
+    TARGET="${_LOUHI_TAG_COMPONENTS[3]}"
+    ARCH="${_LOUHI_TAG_COMPONENTS[4]}"
+    export ARTIFACT_REGISTRY_PROJECT="${_STAGING_ARTIFACTS_PROJECT_ID}"  # Louhi is responsible for passing this.
+    EXT=$(yaml project.yaml "['targets']['${TARGET}']['package_extension']")
+    if [[ "${EXT}" == "deb" ]]; then
+      export REPO_CODENAME="${TARGET}-${ARCH}"
+    fi
+  fi
+}
