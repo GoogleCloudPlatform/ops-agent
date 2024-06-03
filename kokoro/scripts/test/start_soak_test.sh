bc38518b06df5a8c01175b11334ee01680736bd2
Corey Sarsfield <subbarker@gmail.com>
2024-06-03 13:12:34 -0400

    move louhi tag parse before cd

--- kokoro/scripts/test/start_soak_test.sh
+++ kokoro/scripts/test/start_soak_test.sh
@@ -12,14 +12,6 @@ cd ../../../
 # Source the common utilities.
 source kokoro/scripts/utils/louhi.sh
 
-for GIT_ALIAS in git github; do
-  CANDIDATE_LOCATION="${KOKORO_ARTIFACTS_DIR}/${GIT_ALIAS}/unified_agents/integration_test/soak_test/cmd/launcher"
-  if [[ -d "${CANDIDATE_LOCATION}" ]]; then
-    cd "${CANDIDATE_LOCATION}"
-    break
-  fi
-done
-
 # For soak tests run by Louhi
 read_louhi_tag
 # if TARGET & ARCH are set, retrieve the soak distro from project.yaml
@@ -29,6 +21,14 @@ if [[ -n "${TARGET:-}" && -n "${ARCH:-}" ]]; then
   export DISTRO
   export VM_NAME
 fi
+
+for GIT_ALIAS in git github; do
+  CANDIDATE_LOCATION="${KOKORO_ARTIFACTS_DIR}/${GIT_ALIAS}/unified_agents/integration_test/soak_test/cmd/launcher"
+  if [[ -d "${CANDIDATE_LOCATION}" ]]; then
+    cd "${CANDIDATE_LOCATION}"
+    break
+  fi
+done
 
 LOG_RATE=${LOG_RATE-1000} \
 LOG_SIZE_IN_BYTES=${LOG_SIZE_IN_BYTES-1000} \
