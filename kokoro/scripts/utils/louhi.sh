ed4eabf4b1421c66b51fac5a8d4072688eb76871
Corey Sarsfield <subbarker@gmail.com>
2024-06-06 00:59:07 -0400

    initial commit

--- kokoro/scripts/utils/louhi.sh
+++ kokoro/scripts/utils/louhi.sh
@@ -28,14 +28,17 @@ function populate_env_vars_from_louhi_tag_if_present() {
   # Example value: louhi/2.46.0/shortref/windows/x86_64/start
   if [[ -n "${_LOUHI_TAG_NAME:-}" ]]; then
     local -a _LOUHI_TAG_COMPONENTS=(${_LOUHI_TAG_NAME//\// })  
-    export REPO_SUFFIX="${_LOUHI_TAG_COMPONENTS[2]}"  # the shortref is the repo suffix
     TARGET="${_LOUHI_TAG_COMPONENTS[3]}"
     ARCH="${_LOUHI_TAG_COMPONENTS[4]}"
-    export ARTIFACT_REGISTRY_PROJECT="${_STAGING_ARTIFACTS_PROJECT_ID}"  # Louhi is responsible for passing this.
 
-    EXT=$(yaml project.yaml "['targets']['${TARGET}']['package_extension']")
-    if [[ "${EXT}" == "deb" ]]; then
-      export REPO_CODENAME="${TARGET}-${ARCH}"
+    if [[ "${_USE_TEST_REPO:-1}" == "1" ]]; then
+      export REPO_SUFFIX="${_LOUHI_TAG_COMPONENTS[2]}"  # the shortref is the repo suffix
+      export ARTIFACT_REGISTRY_PROJECT="${_STAGING_ARTIFACTS_PROJECT_ID}"  # Louhi is responsible for passing this.
+
+      EXT=$(yaml project.yaml "['targets']['${TARGET}']['package_extension']")
+      if [[ "${EXT}" == "deb" ]]; then
+        export REPO_CODENAME="${TARGET}-${ARCH}"
+      fi
     fi
   fi
 }
