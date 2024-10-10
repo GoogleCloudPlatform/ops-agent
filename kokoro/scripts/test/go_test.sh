0f4e6470bfa5d68481044ed8ac87a7c88c817adf
Corey Sarsfield <subbarker@gmail.com>
2024-10-10 16:32:34 -0400

    Internal: track distro/arch in invocation details  (#1807)

--- kokoro/scripts/test/go_test.sh
+++ kokoro/scripts/test/go_test.sh
@@ -100,6 +100,9 @@ function set_image_specs() {
   export IMAGE_SPECS
 }
 
+export_to_sponge_config "TARGET" "${TARGET:-}"
+export_to_sponge_config "ARCH" "${ARCH:-}"
+
 # Note: if we ever need to change regions, we will need to set up a new
 # Cloud Router and Cloud NAT gateway for that region. This is because
 # we use --no-address on Kokoro, because of b/169084857.
