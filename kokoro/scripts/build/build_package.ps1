bf08bac7220227e2401c64228de4dbd94ec98756
avilevy <avilevy@google.com>
2024-10-17 22:54:40 +0000

    bug fix

--- kokoro/scripts/build/build_package.ps1
+++ kokoro/scripts/build/build_package.ps1
@@ -72,7 +72,7 @@ if ($env:_LOUHI_TAG_NAME -ne $null) {
   $arch=$louhi_tag_components[4]
   $gcs_bucket="gs://${env:_STAGING_ARTIFACTS_PROJECT_ID}-ops-agent-releases/${ver}/${ref}/${target}/${arch}/"
 }
-Write-Host 'gcs_bucket: ${$gcs_bucket}'
+Write-Host 'gcs_bucket: ${gcs_bucket}'
 
 $cache_location="${artifact_registry}/stackdriver-test-143416/google-cloud-ops-agent-build-cache/ops-agent-cache:windows-${arch}${suffix}"
 Invoke-Program docker pull $cache_location
