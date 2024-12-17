fbc4825cd9c72ef100742adf418df76cccccf389
Ida Hou <xuechun@google.com>
2024-12-17 17:46:37 +0000

    Took config generation out of goroutine. Failures in config generation now causes Start() to return an RPC error. Context will be cancelled on config generation failures, resulting in GetStatus() returning an inactive status.

--- cmd/ops_agent_uap_wrapper/plugin.go
+++ cmd/ops_agent_uap_wrapper/plugin.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"flag"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	pb "github.com/GoogleCloudPlatform/ops-agent/cmd/ops_agent_uap_wrapper/google_guest_agent/plugin"
 	"github.com/GoogleCloudPlatform/ops-agent/internal/logs"
 	"google.golang.org/grpc"
 )
+
+// We are able to compute UAP Plugin State Directory in advance, instead of receiving it through Start().
+const OpsAgentUapPluginLog = "ops-agent-uap-plugin.log"
+const UapPluginStateDir = "/var/log/google-cloud-ops-agent"
 
 var (
 	// protocol is the protocol to use tcp/uds.
@@ -49,7 +55,7 @@ func main() {
 	server := grpc.NewServer()
 	defer server.GracefulStop()
 
-	ps := &OpsAgentPluginServer{server: server, logger: logs.Default()}
+	ps := &OpsAgentPluginServer{server: server, logger: CreateOpsAgentUapPluginLogger(UapPluginStateDir, OpsAgentUapPluginLog)}
 	// Successfully registering the server and starting to listen on the address
 	// offered mean Guest Agent was successful in installing/launching the plugin
 	// & will manage the lifecycle (start, stop, or revision change) here onwards.
@@ -62,8 +68,6 @@ func main() {
 	ctx := context.Background()
 	log.Println("Starting Ops Agent UAP Plugin")
 	ps.Start(ctx, &pb.StartRequest{Config: &pb.StartRequest_Config{StateDirectoryPath: "/var/log/google-cloud-ops-agent"}})
-	
-	//ps.Start(ctx, &pb.StartRequest{})
 	for {
 		status, _ := ps.GetStatus(ctx, &pb.GetStatusRequest{})
 		log.Print(status)
@@ -71,6 +75,28 @@ func main() {
 			break
 		}
 		time.Sleep(30 * time.Second)
-
 	}
+}
+
+func CreateOpsAgentUapPluginLogger(logDir string, fileName string) logs.StructuredLogger {
+	// Check if the directory already exists
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		// Directory does not exist, create it
+		err := os.Mkdir(logDir, 0755) // 0755 sets permissions (read/write/execute for owner, read/execute for group and others)
+		if err != nil {
+			log.Printf("failed to create directory for %q: %v", logDir, err)
+			logDir = ""
+		}
+	}
+
+	// Create the log file under the directory
+	path := filepath.Join(logDir, fileName)
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("failed to open health checks log file %q: %v", path, err)
+		return logs.Default()
+	}
+	file.Close()
+
+	return logs.New(path)
 }
