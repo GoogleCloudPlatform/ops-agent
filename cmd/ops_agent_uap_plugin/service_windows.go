5adc447483c15f9f1cb7d603880621879a670cab
Ida Hou <xuechun@google.com>
2025-02-19 16:43:16 +0000

    implemented Stop and GetStatus

--- cmd/ops_agent_uap_plugin/service_windows.go
+++ cmd/ops_agent_uap_plugin/service_windows.go
@@ -19,7 +19,10 @@ package main
 
 import (
 	"context"
+	"log"
 	"os/exec"
+
+	"google.golang.org/grpc/status"
 
 	pb "github.com/GoogleCloudPlatform/ops-agent/cmd/ops_agent_uap_plugin/google_guest_agent/plugin"
 )
@@ -29,14 +32,14 @@ import (
 // between Plugin and the server itself. For e.g. service might want to update
 // plugin config to enable/disable feature here plugins can react to such requests.
 func (ps *OpsAgentPluginServer) Apply(ctx context.Context, msg *pb.ApplyRequest) (*pb.ApplyResponse, error) {
-	panic("Apply method is not implemented on Windows yet")
+	return &pb.ApplyResponse{}, nil
 }
 
 // Start starts the plugin and initiates the plugin functionality.
 // Until plugin receives Start request plugin is expected to be not functioning
 // and just listening on the address handed off waiting for the request.
 func (ps *OpsAgentPluginServer) Start(ctx context.Context, msg *pb.StartRequest) (*pb.StartResponse, error) {
-	panic("Start method is not implemented on Windows yet")
+	return nil, status.Errorf(1, "not implemented on Windows yet")
 }
 
 // Stop is the stop hook and implements any cleanup if required.
@@ -44,7 +47,16 @@ func (ps *OpsAgentPluginServer) Start(ctx context.Context, msg *pb.StartRequest)
 // For e.g. if plugins want to stop some task it was performing or remove some
 // state before exiting it can be done on this request.
 func (ps *OpsAgentPluginServer) Stop(ctx context.Context, msg *pb.StopRequest) (*pb.StopResponse, error) {
-	panic("Stop method is not implemented on Windows yet")
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+	if ps.cancel == nil {
+		log.Printf("The Ops Agent plugin is stopped already, skipping the current request")
+		return &pb.StopResponse{}, nil
+	}
+	log.Printf("Received a Stop request: %s. Stopping the Ops Agent", msg)
+	ps.cancel()
+	ps.cancel = nil
+	return &pb.StopResponse{}, nil
 }
 
 // GetStatus is the health check agent would perform to make sure plugin process
@@ -53,7 +65,15 @@ func (ps *OpsAgentPluginServer) Stop(ctx context.Context, msg *pb.StopRequest) (
 // plugins detect some non-fatal errors causing it unable to offer some features
 // it can reported in status which is sent back to the service by agent.
 func (ps *OpsAgentPluginServer) GetStatus(ctx context.Context, msg *pb.GetStatusRequest) (*pb.Status, error) {
-	panic("GetStatus method is not implemented on Windows yet")
+	log.Println("Received a GetStatus request")
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+	if ps.cancel == nil {
+		log.Println("The Ops Agent plugin is not running")
+		return &pb.Status{Code: 1, Results: []string{"The Ops Agent Plugin is not running."}}, nil
+	}
+	log.Println("The Ops Agent plugin is running")
+	return &pb.Status{Code: 0, Results: []string{"The Ops Agent Plugin is running ok."}}, nil
 }
 
 func runCommand(cmd *exec.Cmd) (string, error) {
