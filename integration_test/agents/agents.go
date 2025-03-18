9b5d5abcc46ce9583ca2f21e7ceea68d90e1b1f5
Ida Hou <xuechun@google.com>
2025-03-18 01:25:32 +0000

    fixed variable init

--- integration_test/agents/agents.go
+++ integration_test/agents/agents.go
@@ -238,7 +238,7 @@ func getOpsAgentLogFilesList(imageSpec string) []string {
 
 func runOpsAgentDiagnosticsWindows(ctx context.Context, logger *logging.DirectoryLogger, vm *gce.VM) {
 	uapWindowsPluginStateDir := `C:\ProgramData\Google\Compute Engine\google-guest-agent\agent_state\plugins\ops-agent-plugin\`
-	stateDir = `C:\ProgramData\Google\Cloud Operations\Ops Agent\`
+	stateDir := `C:\ProgramData\Google\Cloud Operations\Ops Agent\`
 	if gce.IsOpsAgentUAPPlugin() {
 		stateDir = uapWindowsPluginStateDir
 		gce.RunRemotely(ctx, logger.ToFile("ops_agent_uap_plugin_logs.txt"), vm, "Get-WinEvent -FilterHashtable @{ Logname='Application'; ProviderName='google-cloud-ops-agent-uap-plugin' } | Format-Table -AutoSize -Wrap")
