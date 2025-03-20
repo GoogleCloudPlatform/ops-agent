2e02338a3c41b94cad3f39c376f0bcb4471c7588
Ida Hou <xuechun@google.com>
2025-03-20 19:56:22 +0000

    dont throw errors if the scheduled task is not found

--- integration_test/agents/testdata/start-uap-plugin-server.ps1
+++ integration_test/agents/testdata/start-uap-plugin-server.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $taskName = "UAPWindowsPlugin"
-if (-not(Get-ScheduledTask -TaskName $taskName)) {
+if (-not(Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue)) {
     $action = New-ScheduledTaskAction -Execute "C:\plugin.exe" -Argument "-address localhost:1234 -errorlogfile errorlog.txt -protocol tcp" -WorkingDirectory "C:\"
     $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
     $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
