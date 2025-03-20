9bb9bf36b88d70d15e22b9ef5097a88300a33257
Ida Hou <xuechun@google.com>
2025-03-20 18:02:02 +0000

    start the scheduled task directly if exists

--- integration_test/agents/testdata/start-uap-plugin-server.ps1
+++ integration_test/agents/testdata/start-uap-plugin-server.ps1
@@ -1,7 +1,11 @@
 $ErrorActionPreference = 'Stop'
-$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$action = New-ScheduledTaskAction -Execute "C:\plugin.exe" -Argument "-address localhost:1234 -errorlogfile errorlog.txt -protocol tcp" -WorkingDirectory "C:\"
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
-$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-Register-ScheduledTask -TaskName "UAPWindowsPlugin" -Action $action  -Principal $principal
-Start-ScheduledTask -TaskName "UAPWindowsPlugin"
+$taskName = "UAPWindowsPlugin"
+if (-not(Get-ScheduledTask -TaskName $taskName)) {
+    $action = New-ScheduledTaskAction -Execute "C:\plugin.exe" -Argument "-address localhost:1234 -errorlogfile errorlog.txt -protocol tcp" -WorkingDirectory "C:\"
+    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
+    $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+    Register-ScheduledTask -TaskName $taskName -Action $action  -Principal $principal
+}
+Start-ScheduledTask -TaskName $taskName
+
+
