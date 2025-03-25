e610b5542e555fbe4e7f0d61555f51044fbad91d
avilevy <avilevy@google.com>
2025-03-25 17:43:20 +0000

    Testing

--- integration_test/ops_agent_test/main_test.go
+++ integration_test/ops_agent_test/main_test.go
@@ -2513,6 +2513,10 @@ func testDefaultMetrics(ctx context.Context, t *testing.T, logger *log.Logger, v
 
 		var series *monitoringpb.TimeSeries
 		series, err = gce.WaitForMetric(ctx, logger, vm, metric.Type, window, nil, false)
+		if vm.OS.ID == "debian" {
+			time.Sleep(4 * time.Hour)
+		}
+
 		if err != nil {
 			t.Fatal(err)
 		}
