ced190ed90aa9054c54ea41daef25aa8d6262c7e
Ida Hou <xuechun@google.com>
2025-03-26 14:47:38 +0000

    fixed plugin tarball naming

--- pkg/plugin/build.ps1
+++ pkg/plugin/build.ps1
@@ -33,7 +33,7 @@ New-Item -ItemType Directory -Path $Subfolder -Force | Out-Null
 "license to be added" | Out-File "$LicenseDir\text1.txt"
 "license to be added" | Out-File "$Subfolder\text2.txt"
 
-$TarFileName = "google-cloud-ops-agent-plugin-$PkgVersion-windows-$Arch.tar.gz" # Define tar file name
+$TarFileName = "google-cloud-ops-agent-plugin_$PkgVersion-windows-$Arch.tar.gz" # Define tar file name
 
 $FilesToInclude = @(
     "msvcp140.dll",
