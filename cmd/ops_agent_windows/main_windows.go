f09c2a9a627c01ba9803c913bc42e62d94b4d2b9
Ida Hou <xuechun@google.com>
2025-04-08 02:19:11 +0000

    log install/uninstall errors to windows event log

--- cmd/ops_agent_windows/main_windows.go
+++ cmd/ops_agent_windows/main_windows.go
@@ -25,6 +25,7 @@ import (
 	"github.com/GoogleCloudPlatform/ops-agent/internal/logs"
 	"github.com/kardianos/osext"
 	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/eventlog"
 )
 
 const dataDirectory = `Google/Cloud Operations/Ops Agent`
@@ -40,6 +41,16 @@ var (
 func main() {
 
 	infoLog := logs.NewSimpleLogger()
+	identifier := "google-cloud-ops-agent"
+	eventlog.InstallAsEventCreate(identifier, eventlog.Error|eventlog.Warning|eventlog.Info)
+	elog, err := eventlog.Open(identifier)
+	if err != nil {
+		// probably futile
+		log.Fatalf("failed to open event log: %v", err)
+	}
+	defer elog.Close()
+	eventId := uint32(17)
+
 	if ok, err := svc.IsWindowsService(); ok && err == nil {
 		if err := run(serviceName); err != nil {
 			log.Fatal(err)
@@ -55,12 +66,14 @@ func main() {
 		if *installServices {
 			if err := install(); err != nil {
 				infoLog.Printf("error install: %v", err)
+				elog.Error(eventId, fmt.Sprintf("error installing services: %v", err))
 				log.Fatal(err)
 			}
 			infoLog.Printf("installed services")
 		} else if *uninstallServices {
 			if err := uninstall(); err != nil {
 				infoLog.Printf("error uninstall: %v", err)
+				elog.Error(eventId, fmt.Sprintf("error uninstalling services: %v", err))
 				log.Fatal(err)
 			}
 			infoLog.Printf("uninstalled services")
