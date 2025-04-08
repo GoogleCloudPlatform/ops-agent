07c68bc4a63b61c590183e767b179e35ca738a86
Ida Hou <xuechun@google.com>
2025-04-08 03:47:02 +0000

    delete the diagnostics windows Service during install

--- cmd/ops_agent_windows/install_windows.go
+++ cmd/ops_agent_windows/install_windows.go
@@ -40,6 +40,21 @@ func install() error {
 		return err
 	}
 	defer m.Disconnect()
+
+	diagnosticsServiceHandle, err := m.OpenService("google-cloud-ops-agent-diagnostics")
+	// err == nil means the service exists.
+	// If err != nil, the service does not exist, so nothing to delete.
+	if err == nil {
+		defer diagnosticsServiceHandle.Close()
+		if err := stopService(diagnosticsServiceHandle, 30*time.Second); err != nil {
+			return fmt.Errorf("failed to stop the diagnostics Windows service: %w", err)
+		}
+		if err := diagnosticsServiceHandle.Delete(); err != nil {
+			// Don't return until all services have been processed.
+			return fmt.Errorf("fails to delete the diagnostics Windows service: %w", err)
+		}
+	}
+
 	handles := make([]*mgr.Service, len(services))
 	for i, s := range services {
 		// Registering with the event log is required to suppress the "The description for Event ID 1 from source Google Cloud Ops Agent cannot be found" message in the logs.
@@ -90,10 +105,11 @@ func install() error {
 		}, 60)
 		handles[i] = serviceHandle
 	}
+
 	// Automatically (re)start the Ops Agent service.
 	for i := len(services) - 1; i >= 0; i-- {
 		if err := stopService(handles[i], 30*time.Second); err != nil {
-			return err
+			return fmt.Errorf("failed to stop service: %v, error: %v", services[i].name, err)
 		}
 	}
 	return handles[0].Start()
