965127318c56acacf2b1096c5e1a9bc0404e6b7b
avilevy <avilevy@google.com>
2025-06-26 20:04:44 +0000

    Update

--- kokoro/scripts/build/package_windows.sh
+++ kokoro/scripts/build/package_windows.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
-echo "working dir $(pwd)"
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -a Arch"
+   echo -e "\t-a Arch of the binary, must be x86_64 or x86"
+   exit 1 # Exit script after printing help
+}
+
+Arch="x86_64"
+
+if [[ -n $_LOUHI_TAG_NAME ]]
+then
+  # Example value: louhi/2.46.0/abcdef/windows/x86_64/start
+  IFS="/"
+  read -ra louhi_tag_components <<< "$_LOUHI_TAG_NAME"
+  Arch="${louhi_tag_components[4]}"
+else
+  while getopts "a" opt
+  do
+     case "$opt" in
+        a ) Arch="$OPTARG" ;;
+        * ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+     esac
+  done
+fi
+
+# Set GOARCH based on ARCH.
+case $Arch in
+    "x86_64")
+      GoArch="amd64"
+      ;;
+    "x86")
+      GoArch="386"
+      ;;
+    *)
+      echo -e "\tArch must be set to one of: x86, x86_64"
+      ;;
+esac
+
 git config --global --add safe.directory "$(pwd)"
 
 go install -trimpath -ldflags="-s -w" github.com/google/googet/v2/goopack@latest
@@ -22,20 +60,20 @@ releaseName=$(awk -F "=" '/PKG_VERSION/ {print $2}' ./VERSION | tr -d '"')
 
 "$GOPATH"/bin/goopack -output_dir "${KOKORO_ARTIFACTS_DIR}/result" \
   -var:PKG_VERSION="$releaseName" \
-  -var:ARCH=x86_64 \
+  -var:ARCH="$Arch" \
   -var:GOOS=windows \
-  -var:GOARCH=amd64 \
+  -var:GOARCH="$GoArch" \
   pkg/goo/google-cloud-ops-agent.goospec
 
-#if [[ -n $_LOUHI_TAG_NAME ]]
-#then
-#  # Example value: louhi/2.46.0/abcdef/windows/x86_64/start
-#  IFS="/"
-#  read -ra louhi_tag_components <<< "$_LOUHI_TAG_NAME"
-#  ver="${louhi_tag_components[1]}"
-#  ref="${louhi_tag_components[2]}"
-#  target="${louhi_tag_components[3]}"
-#  arch="${louhi_tag_components[4]}"
-#  gcs_bucket="gs://${_STAGING_ARTIFACTS_PROJECT_ID}-ops-agent-releases/${ver}/${ref}/${target}/${arch}/"
-#  gsutil cp "${KOKORO_ARTIFACTS_DIR}"/result/*.goo  "${gcs_bucket}"
-#fi
+if [[ -n $_LOUHI_TAG_NAME ]]
+then
+  # Example value: louhi/2.46.0/abcdef/windows/x86_64/start
+  IFS="/"
+  read -ra louhi_tag_components <<< "$_LOUHI_TAG_NAME"
+  ver="${louhi_tag_components[1]}"
+  ref="${louhi_tag_components[2]}"
+  target="${louhi_tag_components[3]}"
+  Arch="${louhi_tag_components[4]}"
+  gcs_bucket="gs://${_STAGING_ARTIFACTS_PROJECT_ID}-ops-agent-releases/${ver}/${ref}/${target}/${Arch}/"
+  gsutil cp "${KOKORO_ARTIFACTS_DIR}"/result/*.goo  "${gcs_bucket}"
+fi
