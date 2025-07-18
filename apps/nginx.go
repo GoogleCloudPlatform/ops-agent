bfea147d237f70f3f21eb3959fcd0d567b3e1438
Caleb-Hurshman <caleb.hurshman@observiq.com>
2025-07-18 15:59:44 +0000

    genericAccessLogParserV2

--- apps/nginx.go
+++ apps/nginx.go
@@ -72,27 +72,7 @@ func (LoggingProcessorMacroNginxAccess) Type() string {
 }
 
 func (p LoggingProcessorMacroNginxAccess) Expand(ctx context.Context) []confgenerator.InternalLoggingProcessor {
-	return []confgenerator.InternalLoggingProcessor{
-		confgenerator.LoggingProcessorParseRegex{
-			// Documentation:
-			// https://docs.nginx.com/nginx/admin-guide/monitoring/logging/#setting-up-the-access-log
-			// Sample "common" line: 127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
-			// Sample "combined" line: ::1 - - [26/Aug/2021:16:49:43 +0000] "GET / HTTP/1.1" 200 10701 "-" "curl/7.64.0"
-			Regex: `^(?<http_request_remoteIp>[^ ]*) (?<host>[^ ]*) (?<user>[^ ]*) \[(?<time>[^\]]*)\] "(?<http_request_requestMethod>\S+)(?: +(?<http_request_requestUrl>[^\"]*?)(?: +(?<http_request_protocol>\S+))?)?" (?<http_request_status>[^ ]*) (?<http_request_responseSize>[^ ]*)(?: "(?<http_request_referer>[^\"]*)" "(?<http_request_userAgent>[^\"]*)")?(?: "(?<gzip_ratio>[^\"]*)")?$`,
-			ParserShared: confgenerator.ParserShared{
-				TimeKey:    "time",
-				TimeFormat: "%d/%b/%Y:%H:%M:%S %z",
-				Types: map[string]string{
-					"http_request_status": "integer",
-				},
-			},
-		},
-		confgenerator.LoggingProcessorModifyFields{
-			Fields: map[string]*confgenerator.ModifyField{
-				InstrumentationSourceLabel: instrumentationSourceValue(p.Type()),
-			},
-		},
-	}
+	return genericAccessLogParserV2(ctx, p.Type(), "nginx_access", "nginx_access")
 }
 
 type LoggingProcessorMacroNginxError struct {
@@ -143,20 +123,10 @@ func (p LoggingProcessorMacroNginxError) Expand(ctx context.Context) []confgener
 	}
 }
 
-type LoggingReceiverNginxAccess struct {
-	LoggingProcessorMacroNginxAccess `yaml:",inline"`
-	ReceiverMixin                    confgenerator.LoggingReceiverFilesMixin `yaml:",inline" validate:"structonly"`
-}
-
 func loggingReceiverFilesMixinNginxAccess() confgenerator.LoggingReceiverFilesMixin {
 	return confgenerator.LoggingReceiverFilesMixin{
 		IncludePaths: []string{"/var/log/nginx/access.log"},
 	}
-}
-
-type LoggingReceiverNginxError struct {
-	LoggingProcessorMacroNginxError `yaml:",inline"`
-	ReceiverMixin                   confgenerator.LoggingReceiverFilesMixin `yaml:",inline" validate:"structonly"`
 }
 
 func loggingReceiverFilesMixinNginxError() confgenerator.LoggingReceiverFilesMixin {
