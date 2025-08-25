9609803570a862aebc99916b92987d2b9c1e9c82
Rafael Westphal <westphalrafael@google.com>
2025-08-25 16:35:26 +0000

    Adding feature-gate to remove starttime adjustment

--- cmd/ops_agent_uap_plugin/service_linux.go
+++ cmd/ops_agent_uap_plugin/service_linux.go
@@ -194,6 +194,7 @@ func runSubagents(ctx context.Context, cancel context.CancelFunc, pluginInstallD
 	runOtelCmd := exec.CommandContext(ctx,
 		path.Join(pluginInstallDirectory, OtelBinary),
 		"--config", path.Join(pluginStateDirectory, OtelRuntimeDirectory, "otel.yaml"),
+		"--feature-gates=receiver.prometheusreceiver.RemoveStartTimeAdjustment",
 	)
 	wg.Add(1)
 	go runSubAgentCommand(ctx, cancel, runOtelCmd, runCommand, &wg)
