455986a0d3ccd0918d172ca6c7cb5cc2d94c0aaf
Francisco Valente <1435136+franciscovalentecastro@users.noreply.github.com>
2025-11-17 22:46:20 +0000

    Update `Concat` implementation.

--- confgenerator/otel/ottl/ottl.go
+++ confgenerator/otel/ottl/ottl.go
@@ -180,8 +180,12 @@ func ExtractPatternsRubyRegex(a Value, pattern string, omitEmptyValues bool) Val
 	return valuef(`ExtractPatternsRubyRegex(%s, %q, %v)`, a, pattern, omitEmptyValues)
 }
 
-func Concat(a Value, b string) Value {
-	return valuef(`Concat(%s, %s)`, a, b)
+func Concat(values []Value, delimiter string) Value {
+	stringValues := []string{}
+	for _, v := range values {
+		stringValues = append(stringValues, v.String())
+	}
+	return valuef(`Concat([%s], "%s")`, strings.Join(stringValues, ","), delimiter)
 }
 
 func ConvertCase(a Value, toCase string) Value {
