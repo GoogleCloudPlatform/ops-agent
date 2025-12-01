29a497ada537e601f37c5c916179a7636c88317f
Francisco Valente <1435136+franciscovalentecastro@users.noreply.github.com>
2025-12-01 23:18:20 +0000

    Use `InstallGcloudIfNeeded`.

--- integration_test/agents/agents.go
+++ integration_test/agents/agents.go
@@ -1077,6 +1077,10 @@ func InstallOpsAgentUAPPlugin(ctx context.Context, logger *log.Logger, vm *gce.V
 //
 // gcsPath must point to a GCS Path to a .tar.gz file to install on the testing VMs.
 func InstallOpsAgentUAPPluginFromGCS(ctx context.Context, logger *log.Logger, vm *gce.VM, gcsPath string) error {
+	if err := gce.InstallGcloudIfNeeded(ctx, logger, vm); err != nil {
+		return err
+	}
+
 	if gce.IsWindows(vm.ImageSpec) {
 		if _, err := gce.RunRemotely(ctx, logger, vm, "New-Item -ItemType directory -Path C:\\agentPlugin"); err != nil {
 			return err
