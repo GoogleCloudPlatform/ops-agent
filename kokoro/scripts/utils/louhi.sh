9e4fe5d44c9a34b6cbd8ec24ee5233cebeb88ef9
avilevy <avilevy@google.com>
2025-12-05 21:29:33 +0000

    Keep _LOUHI_EXECUTION_ID as the REPO_SUFFIX

--- kokoro/scripts/utils/louhi.sh
+++ kokoro/scripts/utils/louhi.sh
@@ -31,7 +31,7 @@ function populate_env_vars_from_louhi_tag_if_present() {
     ARCH="${_ARCH}"
 
     if [[ "${INSTALL_AGENT_UNDER_TEST:-1}" == "1" ]]; then
-      export REPO_SUFFIX="${_RELEASE_ID}"
+      export REPO_SUFFIX="${_LOUHI_EXECUTION_ID}"
       export ARTIFACT_REGISTRY_PROJECT="${_TESTING_ARTIFACTS_PROJECT_ID}"  # Louhi is responsible for passing this.
 
       EXT=$(yaml project.yaml "['targets']['${TARGET}']['package_extension']")
