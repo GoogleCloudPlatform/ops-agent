20b57f881e6951f1535d44b59fe27d76d6b302a2
XuechunHou <xuechun@google.com>
2026-01-30 17:35:26 +0000

    fixed regex

--- transformation_test/transformation_test.go
+++ transformation_test/transformation_test.go
@@ -673,7 +673,7 @@ func sanitizeOtelStacktrace(t *testing.T, input string) string {
 	// Remove goroutine numbers
 	result = regexp.MustCompile(`goroutine \d+`).ReplaceAllString(result, "goroutine N")
 	// Remove timestamps
-	result = regexp.MustCompile(`\d{4}/\d{2}/\d{2}\s\d{2}:\d{2}:\d{2}\.\d+`).ReplaceAllString(result, "YYYY/MM/DD HH:MM:SS.NNNNNNNNN")
+	result = regexp.MustCompile(`\d{4}/\d{2}/\d{2}\s\d{2}:\d{2}:\d{2}`).ReplaceAllString(result, "YYYY/MM/DD HH:MM:SS")
 
 	result = strings.ReplaceAll(result, "\t", "  ")
 	return result
