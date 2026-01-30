08bcdf2d3f473ef6e7b9aa29a1b31ef230ce55a9
XuechunHou <xuechun@google.com>
2026-01-30 20:23:05 +0000

    use fluentbit v0.1.14

--- transformation_test/transformation_test.go
+++ transformation_test/transformation_test.go
@@ -657,7 +657,7 @@ func sanitizeFluentBitStderr(t *testing.T, input string) string {
 	// Only keep "[error]" lines.
 	result := strings.Join(regexp.MustCompile(`(?m)^.*\[error\].*$`).FindAllString(input, -1), "\n")
 	// Remove timestamps
-	result = regexp.MustCompile(`\d{4}/\d{2}/\d{2}\s\d{2}:\d{2}:\d{2}\.\d+`).ReplaceAllString(result, "YYYY/MM/DD HH:MM:SS.NNNNNNNNN")
+	result = regexp.MustCompile(`\d{4}/\d{2}/\d{2}\s\d{2}:\d{2}:\d{2}`).ReplaceAllString(result, "YYYY/MM/DD HH:MM:SS")
 	result = strings.ReplaceAll(result, "\t", "  ")
 	return result
 }
