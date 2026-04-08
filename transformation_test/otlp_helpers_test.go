9e0baba810daca30e4d2b0ea6efaa4e098eb05ab
Rafael Westphal <westphalrafael@google.com>
2026-04-08 13:01:39 +0000

    Fix undefined ContextWithExperiments in transformation_test

--- transformation_test/otlp_helpers_test.go
+++ transformation_test/otlp_helpers_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/GoogleCloudPlatform/ops-agent/confgenerator"
 	"github.com/GoogleCloudPlatform/ops-agent/confgenerator/otel"
+	"github.com/GoogleCloudPlatform/ops-agent/internal/experiments"
 	"github.com/goccy/go-yaml"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/plog/plogotlp"
@@ -27,7 +28,7 @@ func (transformationConfig transformationTest) generateOTelOTLPExporterConfig(ct
 	pi.RID = "my-log-name"
 	pi.Backend = confgenerator.BackendOTel
 
-	ctx = confgenerator.ContextWithExperiments(ctx, map[string]bool{"otlp_exporter": true})
+	ctx = experiments.ContextWithExperiments(ctx, map[string]bool{"otlp_exporter": true})
 
 	rps, pls, err := pi.OTelComponents(ctx)
 	if err != nil {
