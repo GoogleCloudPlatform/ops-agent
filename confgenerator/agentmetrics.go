9f780802bc6650b0788912a37d89e3b625b71a96
Francisco Valente <1435136+franciscovalentecastro@users.noreply.github.com>
2026-07-30 02:01:48 +0000

    Fix `operators` by setting as slice.

--- confgenerator/agentmetrics.go
+++ confgenerator/agentmetrics.go
@@ -566,11 +566,13 @@ func (r AgentSelfMetrics) HealthChecksPipeline(ctx context.Context) otel.Receive
 				"include":  []string{healthChecksPath},
 				"start_at": "beginning",
 				"storage":  fileStorageExtensionType,
-				"operators": map[string]any{
-					"id":   "body",
-					"type": "move",
-					"from": "body",
-					"to":   "body.message",
+				"operators": []map[string]any{
+					{
+						"id":   "body",
+						"type": "move",
+						"from": "body",
+						"to":   "body.message",
+					},
 				},
 			},
 		},
