9dbf9011151080249855a547ce8b18619ef0f0a4
Francisco Valente <1435136+franciscovalentecastro@users.noreply.github.com>
2026-08-27 18:53:03 +0000

    Only keep labels for `system.cpu.time` metric.

--- apps/hostmetrics.go
+++ apps/hostmetrics.go
@@ -294,11 +294,8 @@ func (r MetricsReceiverHostmetrics) Pipelines(ctx context.Context) ([]otel.Recei
 					"cpu": map[string]interface{}{
 						"metrics": map[string]interface{}{
 							"system.cpu.time": map[string]interface{}{
-								"attributes": []string{
-									"cpu", "state",
-								},
-							},
-							"system.cpu.utilization": map[string]interface{}{
+								// We only need to keep this attributes for the system.cpu.time metric.
+								// See https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/49161
 								"attributes": []string{
 									"cpu", "state",
 								},
